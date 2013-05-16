795cfb495a3e0cec17617d63cc018fa95568e002
Matt Phillips <mphillips@law.harvard.edu>
2013-05-15 20:18:02 -0400

    bug fix: wasn't importing httpresponse in service

--- linky/linky/views/service.py
+++ linky/linky/views/service.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render_to_response, get_object_or_404
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.core.context_processors import csrf
 from django.core.urlresolvers import reverse
 from django.views.decorators.csrf import csrf_exempt
