fac74cfd014777bff9795e70660216929f2ecd06
Matt Phillips <mphillips@law.harvard.edu>
2013-05-22 09:48:22 -0400

    added basic groups and perms to syncdb method

--- linky/linky/settings.example.py
+++ linky/linky/settings.example.py
@@ -119,7 +119,8 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'linky'
+    'linky',
+    'south',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
