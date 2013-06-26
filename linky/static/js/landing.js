92f70caf23f5d01dcee8f523c1c68c3193e0963b
Matt Phillips <mphillips@law.harvard.edu>
2013-06-26 17:30:41 -0400

    added copy button truncated title

--- linky/static/js/landing.js
+++ linky/static/js/landing.js
@@ -22,6 +22,15 @@ $(document).ready(function() {
   });
   
   drawLinks();
+
+  var clip = new ZeroClipboard( document.getElementsByClassName("copy-button"), {
+    moviePath: web_base + "/static/js/ZeroClipboard/ZeroClipboard.swf"
+  } );
+
+  clip.on( 'complete', function(client, args) {
+      this.text = "Copied";
+    //this.style.display = 'none';
+  } );
   
 });
 
@@ -102,3 +111,26 @@ function drawLinks() {
     $('#linky-list').fadeIn();
   }
 }
+
+Handlebars.registerHelper ('truncate', function (str, len) {
+        if (str.length > len) {
+            var new_str = str.substr (0, len+1);
+ 
+            while (new_str.length) {
+                var ch = new_str.substr ( -1 );
+                new_str = new_str.substr ( 0, -1 );
+ 
+                if (ch == ' ') {
+                    break;
+                }
+            }
+ 
+            if ( new_str == '' ) {
+                new_str = str.substr ( 0, len );
+            }
+ 
+            return new Handlebars.SafeString ( new_str +'...' ); 
+        }
+        return str;
+    });
+
