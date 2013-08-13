42b5938530fd6e5865e8a01aed42d4d1102ef122
Leonid Grinberg <leonidg@mit.edu>
2013-08-13 16:47:01 -0400

    First pass at FAQ page

--- django/linky/views/common.py
+++ django/linky/views/common.py
@@ -40,6 +40,10 @@ def about(request):
 
     return render_to_response('about.html', context)
 
+def faq(request):
+    """The FAQ page"""
+    return render_to_response('faq.html', {})
+
 def single_linky(request, linky_guid):
     """Given a Linky ID, serve it up. Vetting also takes place here. """
 
