0549fa6f99edeaa9f155b53c4dac35ab814c38a8
ingrid <hi@ingridcheung.com>
2013-09-23 11:48:51 -0400

    Removing character limit in upload form.

--- perma_web/perma/forms.py
+++ perma_web/perma/forms.py
@@ -19,17 +19,17 @@ class regisrtar_member_form(forms.ModelForm):
     error_messages = {
         'duplicate_email': "A user with that email address already exists.",
     }
-    
+
     email = forms.RegexField(label="Email", required=True, max_length=254,
         regex=r'^[\w.@+-]+$',
         help_text = "Letters, digits and @/./+/-/_ only. 254 characters or fewer.",
         error_messages = {
             'invalid': "This value may contain only letters, numbers and "
                          "@/./+/-/_ characters."})
-    
-    
+
+
     password = forms.CharField(label="Password", widget=forms.PasswordInput)
-    
+
     registrar = forms.ModelChoiceField(queryset=Registrar.objects.all(), empty_label=None)
 
     class Meta:
@@ -39,7 +39,7 @@ class regisrtar_member_form(forms.ModelForm):
     def clean_email(self):
         # Since User.email is unique, this check is redundant,
         # but it sets a nicer error message than the ORM.
-        
+
         email = self.cleaned_data["email"]
         try:
             LinkUser.objects.get(email=email)
@@ -59,13 +59,13 @@ class regisrtar_member_form(forms.ModelForm):
             user.save()
 
         return user
-        
-        
+
+
 class regisrtar_member_form_edit(forms.ModelForm):
     """
     stripped down user reg form
     This is mostly a django.contrib.auth.forms.UserCreationForm
-    
+
     This is the edit form, so we strip it down even more
     """
     error_messages = {
@@ -92,20 +92,20 @@ class regisrtar_member_form_edit(forms.ModelForm):
             user.save()
 
         return user
-        
+
 
 class journal_manager_form(forms.ModelForm):
-        
+
     """
     stripped down user reg form
     This is mostly a django.contrib.auth.forms.UserCreationForm
     """
-    
+
     class Meta:
         model = LinkUser
         fields = ["first_name", "last_name", "email", "password"]
-    
-    
+
+
     error_messages = {
         'duplicate_email': "A user with that email address already exists.",
     }
@@ -139,19 +139,19 @@ class journal_manager_form(forms.ModelForm):
             user.save()
 
         return user
-        
+
 class journal_manager_w_registrar_form(forms.ModelForm):
-        
+
     """
     stripped down user reg form
     This is mostly a django.contrib.auth.forms.UserCreationForm
     """
-    
+
     class Meta:
         model = LinkUser
         fields = ["first_name", "last_name", "email", "password", "registrar"]
-    
-    
+
+
     error_messages = {
         'duplicate_email': "A user with that email address already exists.",
     }
@@ -186,13 +186,13 @@ class journal_manager_w_registrar_form(forms.ModelForm):
             user.save()
 
         return user
-        
-        
+
+
 class journal_manager_form_edit(forms.ModelForm):
     """
     stripped down user reg form
     This is mostly a django.contrib.auth.forms.UserCreationForm
-    
+
     This is stripped down even further to match out editing needs
     """
 
@@ -216,19 +216,19 @@ class journal_manager_form_edit(forms.ModelForm):
 
         return user
 
-        
+
 class journal_member_form(forms.ModelForm):
-        
+
     """
     stripped down user reg form
     This is mostly a django.contrib.auth.forms.UserCreationForm
     """
-    
+
     class Meta:
         model = LinkUser
         fields = ["first_name", "last_name", "email", "password"]
-    
-    
+
+
     error_messages = {
         'duplicate_email': "A user with that email address already exists.",
     }
@@ -262,19 +262,19 @@ class journal_member_form(forms.ModelForm):
             user.save()
 
         return user
-        
+
 class journal_member_w_registrar_form(forms.ModelForm):
-        
+
     """
     stripped down user reg form
     This is mostly a django.contrib.auth.forms.UserCreationForm
     """
-    
+
     class Meta:
         model = LinkUser
         fields = ["first_name", "last_name", "email", "password", "registrar"]
-    
-    
+
+
     error_messages = {
         'duplicate_email': "A user with that email address already exists.",
     }
@@ -309,13 +309,13 @@ class journal_member_w_registrar_form(forms.ModelForm):
             user.save()
 
         return user
-        
-        
+
+
 class journal_member_form_edit(forms.ModelForm):
     """
     stripped down user reg form
     This is mostly a django.contrib.auth.forms.UserCreationForm
-    
+
     This is stripped down even further to match out editing needs
     """
 
@@ -348,25 +348,25 @@ class user_reg_form(forms.ModelForm):
     error_messages = {
         'duplicate_email': "A user with that email address already exists.",
     }
-    
+
     email = forms.RegexField(label="Email", required=True, max_length=254,
         regex=r'^[\w.@+-]+$',
         help_text = "Letters, digits and @/./+/-/_ only. 254 characters or fewer.",
         error_messages = {
             'invalid': "This value may contain only letters, numbers and "
                          "@/./+/-/_ characters."})
-    
-    
+
+
     password = forms.CharField(label="Password", widget=forms.PasswordInput)
 
     class Meta:
         model = LinkUser
         fields = ("email", "password")
-    
+
     def clean_email(self):
         # Since User.email is unique, this check is redundant,
         # but it sets a nicer error message than the ORM.
-        
+
         email = self.cleaned_data["email"]
         try:
             LinkUser.objects.get(email=email)
@@ -380,12 +380,12 @@ class user_reg_form(forms.ModelForm):
         if commit:
             user.save()
         return user
-        
+
 class user_form_self_edit(forms.ModelForm):
     """
     stripped down user reg form
     This is mostly a django.contrib.auth.forms.UserCreationForm
-    
+
     This is stripped down even further to match out editing needs
     """
 
@@ -408,10 +408,10 @@ class user_form_self_edit(forms.ModelForm):
             user.save()
 
         return user
-        
+
 
 
 class UploadFileForm(forms.Form):
-    title = forms.CharField(max_length=50, required=True)
+    title = forms.CharField(required=True)
     url = forms.URLField(required=True)
     file  = forms.FileField(required=True)
