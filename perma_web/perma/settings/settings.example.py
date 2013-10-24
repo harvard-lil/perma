ac2bac568fa1e148b59b7f1113a9ffce549080fe
Jack Cushman <jcushman@gmail.com>
2013-10-23 20:25:15 -0400

    Split up settings.example.py.

---
+++ perma_web/perma/settings/settings.example.py
@@ -0,0 +1,32 @@
+# Choose one of these:
+# from settings_dev import *
+# from settings_prod import *
+
+ADMINS = (
+    # ('Your Name', 'your_email@example.com'),
+)
+
+MANAGERS = ADMINS
+
+DATABASES['default']['NAME'] = 'perma'
+DATABASES['default']['USER'] = 'perma'
+DATABASES['default']['PASSWORD'] = 'perma'
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = ''
+
+# Instapaper credentials
+INSTAPAPER_KEY = 'key'
+INSTAPAPER_SECRET = 'secret'
+INSTAPAPER_USER = 'user@example.com'
+INSTAPAPER_PASS = 'pass'
+
+# Google Analytics
+GOOGLE_ANALYTICS_KEY = 'UA-XXXXX-X'
+GOOGLE_ANALYTICS_DOMAIN = 'example.com'
+
+# To populate the from field of emails sent from Perma
+DEFAULT_FROM_EMAIL = 'email@example.com'
+
+# possibly useful to override:
+# GENERATED_ASSETS_STORAGE = '/tmp/perma/assets'
