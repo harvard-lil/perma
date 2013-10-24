cdfdaea5ea574484de35fab15176031749628af3
jcushman <jcushman@gmail.com>
2013-10-24 15:21:44 -0400

    Don't swallow import errors in settings files.

--- perma_web/perma/settings/__init__.py
+++ perma_web/perma/settings/__init__.py
@@ -3,3 +3,4 @@ try:
 except ImportError, e:
     if e.message=='No module named settings':
         raise Exception("Settings module not found. Did you copy settings.example.py to settings.py?")
+    raise e
