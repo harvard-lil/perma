85b0bd724bc64f5a3dbf7f9e52f4f2efa695ba81
Matthew Phillips <labratmatt@vpn11-47.fas.harvard.edu>
2013-11-11 18:10:16 -0500

    moved allowed_hosts to settings example. We'll need to set this in prod.

--- perma_web/perma/settings/settings.example.py
+++ perma_web/perma/settings/settings.example.py
@@ -15,6 +15,10 @@ DATABASES['default']['PASSWORD'] = 'perma'
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = ''
 
+# Hosts/domain names that are valid for this site; required if DEBUG is False
+# See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = []
+
 # Instapaper credentials
 INSTAPAPER_KEY = 'key'
 INSTAPAPER_SECRET = 'secret'
