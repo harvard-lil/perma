5816fcf73963a879787d5b2d2dd9e5ce7ebcfb1b
Jack Cushman <jcushman@gmail.com>
2014-01-30 20:56:31 -0500

    Use warcprox as thread instead of subprocess.

--- perma_web/warc_server/app.py
+++ perma_web/warc_server/app.py
@@ -1,7 +1,11 @@
 # this is a wsgi application that gets included with its own url prefix
 # alongside the main Django app in wsgi.py
 
-import sys, os
+import sys, os, site
+
+# include our third-party libs
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__name__))
+site.addsitedir(os.path.join(PROJECT_ROOT, 'lib'))
 
 # make sure pywb.wbapp finds our globalwb module
 # this is a weird way to do it -- hopefully pywb will change to support passing settings directly
