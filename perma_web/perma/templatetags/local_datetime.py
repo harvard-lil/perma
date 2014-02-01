b7f4f27ee6a835f9cccf3c307a8545ba9ccab066
Jack Cushman <jcushman@gmail.com>
2014-02-01 14:07:14 -0500

    Handle link creation dates in backend.

--- perma_web/perma/templatetags/local_datetime.py
+++ perma_web/perma/templatetags/local_datetime.py
@@ -8,11 +8,14 @@ from django.template.defaultfilters import date as date_filter
 import time
 
 @register.filter
-def local_datetime(datetime):
+def local_datetime(datetime, format_string="M. d, Y, g:i a"):
     """
         Given a date, print Javascript to print local date/time if available.
     """
-    return mark_safe("<script>document.write(localDateTime(%s))</script><noscript>%s</noscript>" % (
+    if not datetime:
+        return ""
+    return mark_safe("<script>document.write(localDateTime(%s, '%s'))</script><noscript>%s</noscript>" % (
         time.mktime(datetime.timetuple()),
-        date_filter(datetime, "DATETIME_FORMAT")
+        format_string,
+        date_filter(datetime, format_string)
     ))
