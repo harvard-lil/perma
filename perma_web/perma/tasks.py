48bc9b6bb848c60d7e9be3356ac86b14bbb10b5b
Jack Cushman <jcushman@gmail.com>
2014-02-05 14:49:13 -0500

    Merge fix.

--- perma_web/perma/tasks.py
+++ perma_web/perma/tasks.py
@@ -11,10 +11,6 @@ import logging
 import robotparser
 import re
 import time
-<<<<<<< HEAD
-=======
-from random import choice
->>>>>>> 69254cf152008d733ed48d4c883facda0b9bc06c
 import oauth2 as oauth
 import warcprox.warcprox as warcprox
 import thread
