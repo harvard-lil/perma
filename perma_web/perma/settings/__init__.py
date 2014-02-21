6cb9f3d6e9f11b63232f53cf5786a4e03c2e97bf
Jack Cushman <jcushman@gmail.com>
2014-02-20 22:03:38 -0500

    Fix env.

--- perma_web/perma/settings/__init__.py
+++ perma_web/perma/settings/__init__.py
@@ -3,6 +3,6 @@ try:
 except ImportError, e:
     if not e.message=='No module named settings':
         raise
-        
+
     # had to comment out this line so `import perma.settings.settings_test` would work even if settings.py didn't exist:
     #raise Exception("Settings module not found. Did you copy settings.example.py to settings.py?")
