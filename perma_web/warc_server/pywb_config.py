496a14e112970525cfa5b4f3fdfd1183bc8e4c40
Ilya Kreymer <ikreymer@gmail.com>
2014-03-17 17:38:26 -0700

    fix pywb package to @lxml branch cleanup pywb_config, use custom handler for removing mod

--- perma_web/warc_server/pywb_config.py
+++ perma_web/warc_server/pywb_config.py
@@ -16,17 +16,13 @@ class Route(archivalrouter.Route):
     def _apply_filters(self, wbrequest, matcher):
         wbrequest.custom_params['guid'] = matcher.group(1)
 
-# prevent timestamp and mod getting added to rewritten urls
+# prevent mod getting added to rewritten urls
+# timestamp already disabled via 'redir_to_exact' flag
 class Url(WbUrl):
     def to_str(self, **overrides):
         overrides['mod'] = ''
         overrides['timestamp'] = ''
         return WbUrl.to_str(self, **overrides)
-
-    def _init_replay(self, url):
-        self.url = url
-        self.type = self.LATEST_REPLAY
-        return True
 
 class Handler(WBHandler):
     def get_wburl_type(self):
@@ -44,25 +40,16 @@ def create_perma_pywb_app():
     cdx_server = create_cdx_server(settings.CDX_SERVER_URL, DEFAULT_RULES)
     index_reader = IndexReader(cdx_server)
 
-    # enable lxml parsing
+    # enable lxml parsing if available
     use_lxml_parser()
 
-
+    # use util func to create the handler
     wb_handler = create_wb_handler(index_reader,
                                    dict(archive_paths=['file://'],
-                                        #wb_handler_class=Handler,
+                                        wb_handler_class=Handler,
                                         buffer_response=True,
                                         redir_to_exact=False),
                                    DEFAULT_RULES)
-
-    # Loads warcs specified in cdx from these locations
-    # resolvers = [replay_resolvers.PrefixResolver('file://', '')]
-
-    # Create rewriting replay handler to rewrite records
-    #replayer = replay_views.RewritingReplayView(resolvers=resolvers,
-    #                                            loader=archiveloader.ArchiveLoader(),
-    #                                            buffer_response=True,
-    #                                            redir_to_exact=False)
 
     # Finally, create wb router
     return archivalrouter.ArchivalRouter(
