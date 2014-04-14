92dc7bdef397d371aa4cfadb5dccee30e1bca0a4
Matt Phillips <mphillips@law.harvard.edu>
2014-04-14 14:16:59 -0400

    Added a test to get each of our URLs that don't require parameters. If we don't get a 500 error, we call it a success.

---
+++ perma_web/perma/tests/test_urls.py
@@ -0,0 +1,20 @@
+from django.test import TestCase, Client
+from django.core.urlresolvers import reverse
+
+from perma.urls import urlpatterns
+
+class UrlsTestCase(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        
+    def test_url_status_codes(self):
+        """
+        A really simple test for 500 errors. We test all views that don't
+        take parameters (it's not easy to guess what params they want).
+        """
+        
+        for urlpattern in urlpatterns:
+            if '?P<' not in urlpattern.regex.pattern and urlpattern.name:
+                response = self.client.get(reverse(urlpattern.name))
+                self.assertNotEqual(response.status_code, 500)
