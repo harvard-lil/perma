e58d82fb9c3932b45e36e991f44b69f2471c78eb
Greg Leppert <greg@leppert.me>
2014-10-24 17:31:02 -0400

    Rename links to archives and add folders

--- perma_web/api/resources.py
+++ perma_web/api/resources.py
@@ -1,9 +1,29 @@
 # myapp/api.py
+from tastypie import fields
 from tastypie.resources import ModelResource
-from perma.models import Link
+from perma.models import LinkUser, Link, Folder, VestingOrg
 
+
+class LinkUserResource(ModelResource):
+    class Meta:
+        resource_name = 'users'
+        queryset = LinkUser.objects.all()
+
+class VestingOrgResource(ModelResource):
+    class Meta:
+        resource_name = 'vesting_orgs'
+        queryset = VestingOrg.objects.all()
 
 class LinkResource(ModelResource):
+    created_by = fields.ForeignKey(LinkUserResource, 'created_by', full=True, null=True, blank=True)
+    vested_by_editor = fields.ForeignKey(LinkUserResource, 'vested_by_editor', full=True, null=True, blank=True)
+    vesting_org = fields.ForeignKey(VestingOrgResource, 'vesting_org', full=True, null=True)
+
     class Meta:
-        resource_name = 'links'
+        resource_name = 'archives'
         queryset = Link.objects.all()
+
+class FolderResource(ModelResource):
+    class Meta:
+        resource_name = 'folders'
+        queryset = Folder.objects.all()
--- perma_web/api/urls.py
+++ perma_web/api/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import *
 from tastypie.api import Api
-from api.resources import LinkResource
+from api.resources import LinkResource, FolderResource
 
 v1_api = Api(api_name='v1')
 v1_api.register(LinkResource(), canonical=True)
+v1_api.register(FolderResource(), canonical=True)
 
 urlpatterns = v1_api.urls
