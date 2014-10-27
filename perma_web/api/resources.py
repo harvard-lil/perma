7ba10027acde0ff947dd0d8da49650c8042a9882
Greg Leppert <greg@leppert.me>
2014-10-27 14:58:04 -0400

    Add current user and limit fields

--- perma_web/api/resources.py
+++ perma_web/api/resources.py
@@ -1,18 +1,55 @@
-# myapp/api.py
+from tastypie.authentication import BasicAuthentication, ApiKeyAuthentication, MultiAuthentication, SessionAuthentication
+from tastypie.authorization import Authorization
 from tastypie import fields
 from tastypie.resources import ModelResource
 from perma.models import LinkUser, Link, Folder, VestingOrg
 
+user_fields = [
+    'id',
+    'first_name',
+    'last_name'
+]
+
+class CurrentUserResource(ModelResource):
+    class Meta:
+        resource_name = 'user'
+        queryset = LinkUser.objects.all()
+        authentication = MultiAuthentication(SessionAuthentication(), BasicAuthentication())
+        authorization = Authorization()
+        list_allowed_methods = []
+        detail_allowed_methods = ['get']
+        fields = user_fields
+
+    def dispatch_list(self, request, **kwargs):
+        return self.dispatch_detail(request, **kwargs)
+
+    def obj_get(self, bundle, **kwargs):
+        '''
+        Always returns the logged in user.
+        '''
+        return bundle.request.user
+
+    def get_resource_uri(self, bundle_or_obj=None, url_name='api_dispatch_list'):
+        bundle_or_obj = None
+        try:
+            return self._build_reverse_url(url_name, kwargs=self.resource_uri_kwargs(bundle_or_obj))
+        except NoReverseMatch:
+            return ''
 
 class LinkUserResource(ModelResource):
     class Meta:
         resource_name = 'users'
         queryset = LinkUser.objects.all()
+        fields = user_fields
 
 class VestingOrgResource(ModelResource):
     class Meta:
         resource_name = 'vesting_orgs'
         queryset = VestingOrg.objects.all()
+        fields = [
+            'id',
+            'name'
+        ]
 
 class LinkResource(ModelResource):
     created_by = fields.ForeignKey(LinkUserResource, 'created_by', full=True, null=True, blank=True)
@@ -22,8 +59,35 @@ class LinkResource(ModelResource):
     class Meta:
         resource_name = 'archives'
         queryset = Link.objects.all()
+        fields = [
+            'guid',
+            'created_by',
+            'creation_timestamp',
+            'submitted_title',
+            'submitted_url',
+            'notes',
+            'vested',
+            'vesting_org',
+            'vested_timestamp',
+            'vested_timestamp',
+            'dark_archived',
+            'dark_archived_robots_txt_blocked'
+        ]
 
 class FolderResource(ModelResource):
     class Meta:
         resource_name = 'folders'
         queryset = Folder.objects.all()
+        fields = [
+            'creation_timestamp',
+            'id',
+            'is_root_folder',
+            'is_shared_folder',
+            'level',
+            'lft',
+            'name',
+            'resource_uri',
+            'rght',
+            'slug',
+            'tree_id'
+        ]
--- perma_web/api/urls.py
+++ perma_web/api/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import *
 from tastypie.api import Api
-from api.resources import LinkResource, FolderResource
+from api.resources import CurrentUserResource, VestingOrgResource, LinkResource, FolderResource
 
 v1_api = Api(api_name='v1')
+v1_api.register(CurrentUserResource(), canonical=True)
+v1_api.register(VestingOrgResource(), canonical=True)
 v1_api.register(LinkResource(), canonical=True)
 v1_api.register(FolderResource(), canonical=True)
 
