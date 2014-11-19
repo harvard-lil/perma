897ad45c1a6d1783861081ee68da2195cc136f31
Jack Cushman <jcushman@gmail.com>
2014-11-19 09:34:03 -0500

    Better gpg cache.

--- perma_web/mirroring/utils.py
+++ perma_web/mirroring/utils.py
@@ -41,21 +41,16 @@ def may_be_mirrored(view_func):
     return wraps(view_func, assigned=available_attrs(view_func))(wrapped_view)
 
 
-### datetime helpers ###
-# TODO: should we use built-in Django serializer instead?
-
-def serialize_datetime(dt):
-    return dt.strftime("%Y-%m-%d %H:%M:%S")
-
-def unserialize_datetime(dt_string):
-    return pytz.UTC.localize(datetime.datetime.strptime(dt_string, "%Y-%m-%d %H:%M:%S"))
-
-
 ### gpg ###
 
-if settings.MIRRORING_ENABLED:
-    gpg = gnupg.GPG(gnupghome=settings.GPG_DIRECTORY)
-    _fingerprint_cache = {}
+# cache some one-time setup stuff
+_gpg = None
+_fingerprint_cache = {}
+def get_gpg():
+    global _gpg
+    if not _gpg:
+        _gpg = gnupg.GPG(gnupghome=settings.GPG_DIRECTORY)
+    return _gpg
 
 def get_fingerprint(key):
     """
@@ -64,14 +59,14 @@ def get_fingerprint(key):
         that yet this run) then returns the fingerprint.
     """
     if key not in _fingerprint_cache:
-        _fingerprint_cache[key] = gpg.import_keys(key).fingerprints[0]
+        _fingerprint_cache[key] = get_gpg().import_keys(key).fingerprints[0]
     return _fingerprint_cache[key]
 
 def sign_message(message, key=settings.GPG_PRIVATE_KEY):
     if not key:
         return message
     fingerprint = get_fingerprint(key)
-    return gpg.sign(message, keyid=fingerprint)
+    return get_gpg().sign(message, keyid=fingerprint)
 
 def read_signed_message(message, key=settings.UPSTREAM_SERVER.get('public_key') if settings.MIRROR_SERVER else settings.GPG_PUBLIC_KEY):
     if not key:
@@ -84,7 +79,7 @@ def read_signed_message(message, key=settings.UPSTREAM_SERVER.get('public_key') 
     if not verified_message:
         # Not in cache, run decode.
         fingerprint = get_fingerprint(key)
-        verified_message = gpg.decrypt(message)  # decrypt instead of verify, even if message isn't encrypted, so we get the message content in verified_message.data
+        verified_message = get_gpg().decrypt(message)  # decrypt instead of verify, even if message isn't encrypted, so we get the message content in verified_message.data
         if verified_message.fingerprint != fingerprint:
             raise Exception("Signature verification failed: fingerprint does not match %s for message %s." % (fingerprint, message))
         verified_message = verified_message.data[:-1]  # strip extra \n that gpg adds
