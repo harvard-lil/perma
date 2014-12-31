e493f2a995f46f2fd0ffec54d058dcee2a0ed6f7
Greg Leppert <greg@leppert.me>
2014-12-30 19:14:37 -0500

    Build out link creation auth tests

--- perma_web/api/tests/test_folder_authorization.py
+++ perma_web/api/tests/test_folder_authorization.py
@@ -5,18 +5,44 @@ from perma.models import Folder, LinkUser
 
 
 class FolderAuthorizationTestCase(ApiResourceTestCase):
+    fixtures = ['fixtures/users.json',
+                'fixtures/api_keys.json']
+
+    def setUp(self):
+        super(FolderAuthorizationTestCase, self).setUp()
+
+        self.registry_member = LinkUser.objects.get(pk=1)
+        self.registrar_member = LinkUser.objects.get(pk=2)
+        self.vesting_member = LinkUser.objects.get(pk=3)
+        self.regular_user = LinkUser.objects.get(pk=4)
+        self.vesting_manager = LinkUser.objects.get(pk=5)
+
+        self.list_url = "{0}/{1}/".format(self.url_base, FolderResource.Meta.resource_name)
 
     ############
     # Creating #
     ############
 
-    @unittest.skip("Pending")
     def test_should_allow_any_logged_in_user_to_create(self):
-        pass
+        count = Folder.objects.count()
+        parent = self.regular_user.folders.first()
 
-    @unittest.skip("Pending")
+        self.assertHttpCreated(
+            self.api_client.post(self.list_url + str(parent.id) + '/folders/',
+                                 data={'name': 'Test Folder'},
+                                 authentication=self.get_credentials(self.regular_user)))
+
+        self.assertEqual(Folder.objects.count(), count+1)
+
     def test_should_reject_create_from_logged_out_user(self):
-        pass
+        count = Folder.objects.count()
+        parent = Folder.objects.first()
+
+        self.assertHttpUnauthorized(
+            self.api_client.post(self.list_url + str(parent.id) + '/folders/',
+                                 data={'name': 'Test Folder'}))
+
+        self.assertEqual(Folder.objects.count(), count)
 
     ###########
     # Viewing #
