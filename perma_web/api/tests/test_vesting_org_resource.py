87bc4572f61940856dcdc65c6724f136853fafd5
Greg Leppert <greg@leppert.me>
2015-01-10 23:25:17 -0500

    Add vesting_org resource test case

--- perma_web/api/tests/test_vesting_org_resource.py
+++ perma_web/api/tests/test_vesting_org_resource.py
@@ -1,13 +1,13 @@
 from .utils import ApiResourceTestCase
 
 
-class RegistrarResourceTestCase(ApiResourceTestCase):
+class VestingOrgResourceTestCase(ApiResourceTestCase):
     fixtures = ['fixtures/users.json',
                 'fixtures/folders.json']
 
     def setUp(self):
-        super(RegistrarResourceTestCase, self).setUp()
-        self.list_url = self.url_base+'/registrars/'
+        super(VestingOrgResourceTestCase, self).setUp()
+        self.list_url = self.url_base+'/vesting_orgs/'
         self.detail_url = self.list_url+'1/'
         self.fields = [
             'id',
@@ -15,10 +15,10 @@ class RegistrarResourceTestCase(ApiResourceTestCase):
         ]
 
     def test_get_list_json(self):
-        self.successful_list_get(self.list_url, 1)
+        self.successful_list_get(self.list_url, 3)
 
     def test_get_detail_json(self):
         self.successful_detail_get(self.detail_url, self.fields)
 
     def test_get_vesting_orgs_json(self):
-        self.successful_list_get(self.detail_url + 'vesting_orgs/', 3)
+        self.successful_list_get(self.detail_url + 'folders/', 1)
