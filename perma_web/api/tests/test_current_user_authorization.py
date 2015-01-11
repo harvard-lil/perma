111c8a6c0adb62b718af1d98469f813d95d81b4f
Greg Leppert <greg@leppert.me>
2015-01-10 22:43:34 -0500

    Add current_user tests

---
+++ perma_web/api/tests/test_current_user_authorization.py
@@ -0,0 +1,47 @@
+from .utils import ApiResourceTestCase
+
+from perma.models import LinkUser
+
+
+class CurrentUserAuthorizationTestCase(ApiResourceTestCase):
+    fixtures = ['fixtures/users.json',
+                'fixtures/folders.json',
+                'fixtures/api_keys.json']
+
+    def setUp(self):
+        super(CurrentUserAuthorizationTestCase, self).setUp()
+        self.vesting_member = LinkUser.objects.get(pk=3)
+        self.regular_user = LinkUser.objects.get(pk=4)
+        self.detail_url = self.url_base+'/user/'
+
+    def test_should_allow_user_to_get_self(self):
+        self.assertHttpOK(
+            self.api_client.get(self.detail_url,
+                                authentication=self.get_credentials(self.vesting_member)))
+
+    def test_get_reject_request_from_unauthenticated_visitor(self):
+        self.assertHttpUnauthorized(self.api_client.get(self.detail_url))
+
+    def test_should_provide_different_detail_data_relative_to_user(self):
+        vm_data = self.deserialize(
+            self.api_client.get(self.detail_url,
+                                authentication=self.get_credentials(self.vesting_member)))
+
+        reg_data = self.deserialize(
+            self.api_client.get(self.detail_url,
+                                authentication=self.get_credentials(self.regular_user)))
+
+        self.assertEqual(vm_data.keys(), reg_data.keys())
+        self.assertNotEqual(vm_data, reg_data)
+
+    def test_should_provide_different_list_data_relative_to_user(self):
+        vm_data = self.deserialize(
+            self.api_client.get(self.detail_url + 'folders/',
+                                authentication=self.get_credentials(self.vesting_member)))
+
+        reg_data = self.deserialize(
+            self.api_client.get(self.detail_url + 'folders/',
+                                authentication=self.get_credentials(self.regular_user)))
+
+        self.assertEqual(vm_data.keys(), reg_data.keys())
+        self.assertNotEqual(vm_data['objects'], reg_data['objects'])
--- perma_web/api/tests/test_current_user_resource.py
+++ perma_web/api/tests/test_current_user_resource.py
@@ -6,11 +6,12 @@ from perma.models import LinkUser
 class CurrentUserResourceTestCase(ApiResourceTestCase):
     fixtures = ['fixtures/users.json',
                 'fixtures/folders.json',
-                'fixtures/api_keys.json']
+                'fixtures/api_keys.json',
+                'fixtures/archive.json']
 
     def setUp(self):
         super(CurrentUserResourceTestCase, self).setUp()
-        self.user = LinkUser.objects.get(email='test_vesting_member@example.com')
+        self.vesting_member = LinkUser.objects.get(pk=3)
         self.detail_url = self.url_base+'/user/'
         self.fields = [
             'id',
@@ -21,13 +22,30 @@ class CurrentUserResourceTestCase(ApiResourceTestCase):
             'groups'
         ]
 
-    def test_get_detail_json(self):
+    def test_get_self_detail_json(self):
         resp = self.api_client.get(self.detail_url,
-                                   format='json',
-                                   authentication=self.get_credentials())
+                                   authentication=self.get_credentials(self.vesting_member))
         self.assertValidJSONResponse(resp)
         obj = self.deserialize(resp)
         self.assertKeys(obj, self.fields)
 
-    def test_get_detail_unauthenticated(self):
-        self.assertHttpUnauthorized(self.api_client.get(self.detail_url, format='json'))
+    def test_get_archives_json(self):
+        resp = self.api_client.get(self.detail_url + 'archives/',
+                                   authentication=self.get_credentials(self.vesting_member))
+        self.assertValidJSONResponse(resp)
+        data = self.deserialize(resp)
+        self.assertEqual(len(data['objects']), 1)
+
+    def test_get_folders_json(self):
+        resp = self.api_client.get(self.detail_url + 'folders/',
+                                   authentication=self.get_credentials(self.vesting_member))
+        self.assertValidJSONResponse(resp)
+        data = self.deserialize(resp)
+        self.assertEqual(len(data['objects']), 2)
+
+    def test_get_vesting_orgs_json(self):
+        resp = self.api_client.get(self.detail_url + 'vesting_orgs/',
+                                   authentication=self.get_credentials(self.vesting_member))
+        self.assertValidJSONResponse(resp)
+        data = self.deserialize(resp)
+        self.assertEqual(len(data['objects']), 1)
