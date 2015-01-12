cf089c8e70969a02e62da9d3666cfcc8fb791dd4
Greg Leppert <greg@leppert.me>
2015-01-12 14:39:00 -0500

    Remove old folder management code

--- perma_web/perma/views/link_management.py
+++ perma_web/perma/views/link_management.py
@@ -1,19 +1,13 @@
 import logging
-import json
-from datetime import datetime
-from django.db import transaction
-from django.contrib import messages
 
-from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
-from django.http import HttpResponseBadRequest, HttpResponseRedirect
-from django.shortcuts import HttpResponse, render_to_response, get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render_to_response, get_object_or_404, render
 from django.template import RequestContext
 
-from ..models import Link, Asset, Folder, VestingOrg, FolderException
-from ..tasks import upload_to_internet_archive
-from ..utils import require_group, run_task
+from ..models import Link, Asset, Folder
+from ..utils import require_group
 
 # The api app conflicts with the legacy api view
 # so we have to import via string.
@@ -56,79 +50,8 @@ def folder_contents(request, folder_id):
     # helper vars
     user = request.user
     folder_access_filter = Folder.objects.user_access_filter(user)
-    link_access_filter = Link.objects.user_access_filter(user)
 
     current_folder = get_object_or_404(Folder, folder_access_filter, pk=folder_id)
-
-    # handle forms
-    if request.POST:
-        class BadRequest(Exception):
-            pass
-
-        out = {'success': 1}
-
-        # we wrap all the edits in a try/except and a transaction, so we can roll back everything if we hit an error
-        try:
-            with transaction.atomic():
-                action = request.POST.get('action')
-
-                # move items
-                if action == 'move_items':
-                    # move links
-                    for link_id in request.POST.getlist('links'):
-                        link = get_object_or_404(Link, link_access_filter, pk=link_id)
-                        if link.vested and link.vesting_org != current_folder.vesting_org:
-                            raise BadRequest("Can't move vested link out of organization's shared folder.")
-                        link.move_to_folder_for_user(current_folder, user)
-
-                    # move folders
-                    for folder_id in request.POST.getlist('folders'):
-                        folder = get_object_or_404(Folder, folder_access_filter, pk=folder_id)
-                        try:
-                            folder.move_to_folder(current_folder)
-                        except FolderException as e:
-                            raise BadRequest(e.message)
-
-                # new folder
-                elif action == 'new_folder':
-                    new_folder = Folder(name=request.POST['name'].strip(), parent=current_folder, created_by=user)
-                    new_folder.save()
-                    out['new_folder_id'] = new_folder.pk
-
-                # rename folder
-                elif action == 'rename_folder':
-                    if current_folder.is_shared_folder or current_folder.is_root_folder:
-                        raise BadRequest("Not a valid action for this folder.")
-                    current_folder.rename(request.POST['name'])
-
-                # delete folder
-                elif action == 'delete_folder':
-                    if current_folder.is_shared_folder or current_folder.is_root_folder:
-                        raise BadRequest("Not a valid action for this folder.")
-                    if not current_folder.is_empty():
-                        raise BadRequest("Folders can only be deleted if they are empty.")
-                    current_folder.delete()
-
-                # save notes
-                elif action == 'save_notes':
-                    link = get_object_or_404(Link, link_access_filter, pk=request.POST['link_id'])
-                    link.notes = request.POST['notes']
-                    link.save()
-
-                # save title change
-                elif action == 'save_title':
-                    link = get_object_or_404(Link, link_access_filter, pk=request.POST['link_id'])
-                    link.submitted_title = request.POST['title']
-                    link.save()
-
-                else:
-                    raise BadRequest("Action not recognized.")
-
-        except BadRequest as e:
-            return HttpResponseBadRequest(e.message)
-
-        if request.is_ajax():
-            return HttpResponse(json.dumps(out), content_type="application/json")
 
     # start with all links belonging to user
     links = Link.objects.accessible_to(user).select_related('vested_by_editor', 'created_by')
