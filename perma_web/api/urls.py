b05325ef8717dc638b27ddc7fa37ac84305bb17f
Jack Cushman <jcushman@gmail.com>
2015-03-09 16:01:00 -0400

    Add "v1a" API endpoint for alpha testing.

--- perma_web/api/urls.py
+++ perma_web/api/urls.py
@@ -1,19 +1,15 @@
-from django.conf import settings
-from tastypie.api import Api
-from api.resources import (VestingOrgResource,
-                           LinkResource,
+from django.conf.urls import patterns, include
+from tastypie.api import Api, NamespacedApi
+from api.resources import (LinkResource,
                            FolderResource,
-                           AssetResource,
-                           RegistrarResource,
                            CurrentUserResource,
                            CurrentUserLinkResource,
                            CurrentUserFolderResource,
                            CurrentUserVestingOrgResource, PublicLinkResource)
 
+### v1 ###
+
 v1_api = Api(api_name='v1')
-# v1_api.register(VestingOrgResource())
-# v1_api.register(AssetResource())
-# v1_api.register(RegistrarResource())
 
 # /public/archives
 v1_api.register(PublicLinkResource())
@@ -35,4 +31,13 @@ v1_api.register(CurrentUserLinkResource())
 v1_api.register(CurrentUserFolderResource())
 v1_api.register(CurrentUserVestingOrgResource())
 
-urlpatterns = v1_api.urls
+### v1a ###
+
+v1a_api = NamespacedApi(api_name='v1a', urlconf_namespace='v1a')
+v1a_api._registry = v1_api._registry.copy()
+v1a_api._canonicals = v1_api._canonicals.copy()
+
+### add API versions to urlpatters ###
+
+urlpatterns = v1_api.urls + v1a_api.urls
+
