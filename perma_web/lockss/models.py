1317f53e3eb26465bfca38a3863accf51837dc64
Jack Cushman <jcushman@gmail.com>
2015-09-02 13:20:56 -0400

    More LOCKSS.

---
+++ perma_web/lockss/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from django.core.cache import cache as django_cache
+
+
+class Mirror(models.Model):
+    name = models.CharField(max_length=255)
+    ip = models.CharField(max_length=15, help_text="E.g. 12.34.56.78")
+    hostname = models.CharField(max_length=255, help_text="E.g. mirror.example.com")
+    peer_port = models.IntegerField(default=9729, help_text="Port where this mirror listens for other LOCKSS nodes.")
+    content_url = models.URLField(max_length=255, help_text="E.g. https://mirror.example.edu:8080/")
+    enabled = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['name']
+
+    def __unicode__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        super(Mirror, self).save(*args, **kwargs)
+        self._invalidate_cached_mirrors()
+
+    def delete(self, *args, **kwargs):
+        super(Mirror, self).delete(*args, **kwargs)
+        self._invalidate_cached_mirrors()
+
+    @classmethod
+    def get_cached_mirrors(cls):
+        """
+            Get a list of (name, content_url) pairs for each mirror, to use when playing back content.
+            Keep result in cache.
+        """
+        # get mirror list from cache
+        mirrors = django_cache.get('mirrors')
+        if mirrors is None:
+            mirrors = [(mirror.name, mirror.content_url) for mirror in cls.objects.filter(enabled=True)]
+            django_cache.set('mirrors', mirrors)
+
+        # return mirrors
+        if mirrors:
+            return mirrors
+        return []
+
+    def _invalidate_cached_mirrors(self):
+        django_cache.delete('mirrors')
