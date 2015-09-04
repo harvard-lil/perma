8802b669f656bdc9e4cc403eea7c5a9d136ffbea
Dano Williams <dan.o.williams@gmail.com>
2015-09-04 16:09:06 -0400

    got the blue bar up

--- perma_web/perma/settings/deployments/settings_common.py
+++ perma_web/perma/settings/deployments/settings_common.py
@@ -208,7 +208,6 @@ PIPELINE_CSS = {
         'source_filenames': (
             'css/bootstrap3.css',
             'css/style-responsive-archive.scss',
-            'css/font-awesome.min.css',
         ),
         'output_filename': 'css/base-archive-bundle.css',
     },
