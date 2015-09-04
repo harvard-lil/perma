18b2f06180fe749f112375759aea106dde087fac
Dano Williams <dan.o.williams@gmail.com>
2015-09-04 10:06:02 -0400

    reinstated adjustHeight() into single-link.js -- didn't work otherwise

--- perma_web/static/js/single-link.js
+++ perma_web/static/js/single-link.js
@@ -2,4 +2,18 @@ $(document).ready(function() {
     $("#details-button").click(function () {
         $(this).text($(this).text() == "Show details" ? "Hide details" : "Show details");
     });    
+
+    adjustHeight();
+    $('#collapse-refresh').on('shown.bs.collapse', function () {
+        adjustHeight();
+    });
+    $('#collapse-refresh').on('hidden.bs.collapse', function () {
+        adjustHeight();
+    });
+
+	function adjustHeight() {
+	    var	headerHeight = $('header').height(),
+	    	windowHeight = $(window).height();
+	    $('iframe').height(windowHeight - headerHeight - 0);
+	}
 });
