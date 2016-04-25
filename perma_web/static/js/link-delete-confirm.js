b76b4c5c97180364bf4058c58ba81530afcd1612
Anastasia <aaizman@vpn-146-44.vpnclient.harvard.edu>
2016-04-25 17:37:55 -0400

    clean up - no real change to link-delete-confirm

--- perma_web/static/js/link-delete-confirm.js
+++ perma_web/static/js/link-delete-confirm.js
@@ -1,25 +1,24 @@
-$(function(){
-    $("button.delete-confirm").click(function(){
-        var $this = $(this),
-            prev_text = $this.text();
+$(document).ready(function(){
+  $('button.delete-confirm').click(function(){
+    var $this = $(this),
+      prev_text = $this.text();
 
-        if (!$this.hasClass("disabled")){
+    if (!$this.hasClass('disabled')){
 
-            $this.addClass("disabled");
-            $this.text("Deleting link...");
+      $this.addClass('disabled');
+      $this.text('Deleting link...');
 
-            Helpers.apiRequest("DELETE", "/archives/" + archive.guid + "/", null, {
-                success: function(){
-                    window.location = url_link_browser + "/?deleted=" + archive.guid;
-                },
-                error: function(jqXHR){
-                    $this.removeClass("disabled");
-                    $this.text(prev_text);
-                    Helpers.showAPIError(jqXHR);
-                }
-            });
+      Helpers.apiRequest('DELETE', '/archives/' + archive.guid + '/', null, {
+        success: function(){
+          window.location = url_link_browser + '/?deleted=' + archive.guid;
+        },
+        error: function(jqXHR){
+          $this.removeClass('disabled');
+          $this.text(prev_text);
+          Helpers.showAPIError(jqXHR);
         }
-
-        return false;
-    });
+      });
+    }
+    return false;
+  });
 });
