9842de90b27d48a64d580ac264510298e8bfa918
Anastasia <aaizman@vpn-146-47.vpnclient.harvard.edu>
2016-05-02 15:56:25 -0400

    remove - allow_relation prop on cdxrouter

--- perma_web/perma/cdx_router.py
+++ perma_web/perma/cdx_router.py
@@ -19,19 +19,6 @@ class CDXRouter(object):
             return 'perma-cdxline'
         return None
 
-    def allow_relation(self, obj1, obj2, **hints):
-        """
-        Return True if a relation between obj1 and obj2 should be allowed
-        False if the relation should be prevented
-        or None if the router has no opinion.
-        This is purely a validation operation, used by foreign key
-        and many to many operations to determine
-        if a relation should be allowed between two objects.
-        """
-        if 'target_db' in hints:
-            return db == hints['target_db']
-        return None
-
     def allow_migrate(self, db, app_label, model_name=None, **hints):
         """
         Make sure only cdx models appear in the 'perma-cdxline'
