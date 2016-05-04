858eaa32f99dc42132e9b9e2ade20cd86ce0598a
Anastasia <aaizman@vpn-146-50.vpnclient.harvard.edu>
2016-05-04 18:03:34 -0400

    update - using rds db instance instead of jawsdb

--- perma_web/perma/settings/deployments/settings_heroku.py
+++ perma_web/perma/settings/deployments/settings_heroku.py
@@ -29,11 +29,11 @@ LOGGING['handlers']['rollbar'] = {
 LOGGING['loggers']['']['handlers'] += ['rollbar']
 
 # Parse database configuration from env DATABASE_URL
-if os.environ.get('JAWSDB_URL', False):
+if os.environ.get('AWS_MYSQL_STAGE_DEFAULT', False):
     # JawsDB
     import dj_database_url
-    DATABASES['default'] = dj_database_url.config('JAWSDB_URL')
-    DATABASES['perma-cdxline'] = dj_database_url.config('JAWSDB_BROWN_URL')
+    DATABASES['default'] = dj_database_url.config('AWS_MYSQL_STAGE_DEFAULT')
+    DATABASES['perma-cdxline'] = dj_database_url.config('AWS_MYSQL_STAGE_CDXLINE')
 else:
     # Amazon RDS
     DATABASES['default']['OPTIONS'] = {'ssl': {'ca': os.path.join(PROJECT_ROOT, 'amazon-rds-combined-ca-bundle.pem')}}
