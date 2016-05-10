a6ff350e4dc4d79ada0d4df78fb937415c6576bd
Jack Cushman <jcushman@gmail.com>
2016-05-10 11:33:26 -0400

    Add template tag for debugging.

---
+++ perma_web/perma/templatetags/set_trace.py
@@ -0,0 +1,14 @@
+from __future__ import print_function, absolute_import
+
+import pdb
+from pprint import pprint
+
+from django import template
+
+register = template.Library()
+
+@register.simple_tag(takes_context=True)
+def set_trace(context):
+    """ Handy for figuring out what's going on in a template. Usage: {% set_trace %}. """
+    pprint(context)
+    pdb.set_trace()
