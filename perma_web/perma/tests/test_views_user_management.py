cdfb2626f49cdb47f010980c39ba5dbbe22614c2
Jack Cushman <jcushman@gmail.com>
2016-05-10 11:37:55 -0400

    Add tests for adding existing user to org.

--- perma_web/perma/tests/test_views_user_management.py
+++ perma_web/perma/tests/test_views_user_management.py
@@ -17,10 +17,12 @@ class UserManagementViewsTestCase(PermaTestCase):
 
         self.admin_user = LinkUser.objects.get(pk=1)
         self.registrar_member = LinkUser.objects.get(pk=2)
+        self.regular_user = LinkUser.objects.get(pk=4)
         self.registrar = self.registrar_member.registrar
         self.unrelated_registrar = Registrar.objects.exclude(pk=self.registrar.pk).first()
-        self.organization = self.registrar.organizations.first()
+        self.organization = Organization.objects.get(pk=1)
         self.organization_member = self.organization.users.first()
+        self.another_organization = Organization.objects.get(pk=2)
         self.unrelated_organization = self.unrelated_registrar.organizations.first()
         self.deletable_organization = Organization.objects.get(pk=3)
 
@@ -205,6 +207,64 @@ class UserManagementViewsTestCase(PermaTestCase):
             self.submit_form('user_management_manage_single_' + view_name + '_delete',
                            reverse_kwargs={'args': [new_user.pk]},
                            success_url=reverse('user_management_manage_' + view_name))
+
+    ### UPGRADING USERS ###
+
+    def test_admin_user_can_add_user_to_org(self):
+        self.log_in_user(self.admin_user)
+        self.submit_form('user_management_organization_user_add_user',
+                         data={'a-organizations': self.organization.pk},
+                         query_params={'email': self.regular_user.email},
+                         success_url=reverse('user_management_manage_organization_user'),
+                         success_query=self.regular_user.organizations.filter(pk=self.organization.pk))
+
+    def test_registrar_user_can_add_user_to_org(self):
+        self.log_in_user(self.registrar_member)
+        self.submit_form('user_management_organization_user_add_user',
+                         data={'a-organizations': self.organization.pk},
+                         query_params={'email': self.regular_user.email},
+                         success_url=reverse('user_management_manage_organization_user'),
+                         success_query=self.regular_user.organizations.filter(pk=self.organization.pk))
+
+    def test_org_user_can_add_user_to_org(self):
+        self.log_in_user(self.organization_member)
+        self.submit_form('user_management_organization_user_add_user',
+                         data={'a-organizations': self.organization.pk},
+                         query_params={'email': self.regular_user.email},
+                         success_url=reverse('user_management_manage_organization_user'),
+                         success_query=self.regular_user.organizations.filter(pk=self.organization.pk))
+
+    def test_registrar_user_cannot_add_user_to_inaccessible_org(self):
+        self.log_in_user(self.registrar_member)
+        self.submit_form('user_management_organization_user_add_user',
+                         data={'a-organizations': self.unrelated_organization.pk},
+                         query_params={'email': self.regular_user.email},
+                         error_keys=['organizations'])
+        self.assertFalse(self.regular_user.organizations.filter(pk=self.unrelated_organization.pk).exists())
+
+    def test_org_user_cannot_add_user_to_inaccessible_org(self):
+        self.log_in_user(self.organization_member)
+        self.submit_form('user_management_organization_user_add_user',
+                         data={'a-organizations': self.another_organization.pk},
+                         query_params={'email': self.regular_user.email},
+                         error_keys=['organizations'])
+        self.assertFalse(self.regular_user.organizations.filter(pk=self.another_organization.pk).exists())
+
+    def test_cannot_add_admin_user_to_org(self):
+        self.log_in_user(self.organization_member)
+        resp = self.submit_form('user_management_organization_user_add_user',
+                         data={'a-organizations': self.organization.pk},
+                         query_params={'email': self.admin_user.email})
+        self.assertIn("is an admin user", resp.content)
+        self.assertFalse(self.admin_user.organizations.exists())
+
+    def test_cannot_add_registrar_user_to_org(self):
+        self.log_in_user(self.organization_member)
+        resp = self.submit_form('user_management_organization_user_add_user',
+                                data={'a-organizations': self.organization.pk},
+                                query_params={'email': self.registrar_member.email})
+        self.assertIn("is a registrar member", resp.content)
+        self.assertFalse(self.registrar_member.organizations.exists())
 
     ### SETTINGS ###
 
