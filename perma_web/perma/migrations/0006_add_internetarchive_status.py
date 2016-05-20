500c3936f78383147049b57cda972621983aede4
Anastasia <anastasia.aizman@gmail.com>
2016-05-20 12:47:13 -0400

    fix - for speed

--- perma_web/perma/migrations/0006_add_internetarchive_status.py
+++ perma_web/perma/migrations/0006_add_internetarchive_status.py
@@ -5,39 +5,24 @@ from django.db import migrations, models
 
 def update_upload_to_ia_field(apps, schema_editor):
     Link = apps.get_model('perma', 'Link')
-    for link in Link.objects.all():
-        if link.uploaded_to_internet_archive:
-            link.internet_archive_upload_status = 'completed'
-        elif not link.uploaded_to_internet_archive:
-            link.internet_archive_upload_status = 'not_started'
-
-        link.save()
-
+    Link.objects.filter(uploaded_to_internet_archive=True).update(internet_archive_upload_status='completed')
+    Link.objects.filter(uploaded_to_internet_archive=False).update(internet_archive_upload_status='not_started')
     HistoricalLink = apps.get_model('perma','HistoricalLink')
-    for hlink in HistoricalLink.objects.all():
-        if hlink.uploaded_to_internet_archive:
-            hlink.internet_archive_upload_status = 'completed'
-        elif not hlink.uploaded_to_internet_archive:
-            hlink.internet_archive_upload_status = 'not_started'
-
-        hlink.save()
-
+    HistoricalLink.objects.filter(uploaded_to_internet_archive=True).update(internet_archive_upload_status='completed')
+    HistoricalLink.objects.filter(uploaded_to_internet_archive=False).update(internet_archive_upload_status='not_started')
 
 def reverse_update_upload_to_ia_field(apps, schema_editor):
     Link = apps.get_model('perma', 'Link')
-    for link in Link.objects.all():
-        if link.internet_archive_upload_status == 'completed':
-            link.uploaded_to_internet_archive = True
-        else:
-            link.uploaded_to_internet_archive = False
-    link.save()
-    HistoricalLink = apps.get_model('perma', 'Link')
-    for hlink in HistoricalLink.objects.all():
-        if hlink.internet_archive_upload_status == 'completed':
-            hlink.uploaded_to_internet_archive = True
-        else:
-            hlink.uploaded_to_internet_archive = False
-    hlink.save()
+    Link.objects.filter(internet_archive_upload_status='completed').update(uploaded_to_internet_archive=True)
+    Link.objects.filter(
+        Q(internet_archive_upload_status='deleted') | Q(internet_archive_upload_status='not_started') | Q(internet_archive_upload_status='failed') | Q(internet_archive_upload_status='failed_permanently')
+        ).update(uploaded_to_internet_archive=False)
+
+    HistoricalLink = apps.get_model('perma', 'HistoricalLink')
+    HistoricalLink.objects.filter(internet_archive_upload_status='completed').update(uploaded_to_internet_archive=True)
+    HistoricalLink.objects.filter(
+        Q(internet_archive_upload_status='deleted') | Q(internet_archive_upload_status='not_started') | Q(internet_archive_upload_status='failed') | Q(internet_archive_upload_status='failed_permanently')
+        ).update(uploaded_to_internet_archive=False)
 
 class Migration(migrations.Migration):
 
@@ -49,12 +34,12 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='historicallink',
             name='internet_archive_upload_status',
-            field=models.CharField(default=b'not_started', max_length=15, choices=[(b'not_started', b'not_started'), (b'completed', b'completed'), (b'failed', b'failed'), (b'failed_permanently', b'failed_permanently'), (b'deleted', b'deleted')]),
+            field=models.CharField(default=b'not_started', max_length=20, choices=[(b'not_started', b'not_started'), (b'completed', b'completed'), (b'failed', b'failed'), (b'failed_permanently', b'failed_permanently'), (b'deleted', b'deleted')]),
         ),
         migrations.AddField(
             model_name='link',
-            name='internet_archive_upload_status',
-            field=models.CharField(default=b'not_started', max_length=15, choices=[(b'not_started', b'not_started'), (b'completed', b'completed'), (b'failed', b'failed'), (b'failed_permanently', b'failed_permanently'), (b'deleted', b'deleted')]),
+            name='internet_arc\hive_upload_status',
+            field=models.CharField(default=b'not_started', max_length=20, choices=[(b'not_started', b'not_started'), (b'completed', b'completed'), (b'failed', b'failed'), (b'failed_permanently', b'failed_permanently'), (b'deleted', b'deleted')]),
         ),
 
         migrations.RunPython(update_upload_to_ia_field, reverse_code=reverse_update_upload_to_ia_field),
