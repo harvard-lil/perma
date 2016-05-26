1275ac1a5a2a396b541ce5cbd40a68ebd31caa1d
Jack Cushman <jcushman@gmail.com>
2016-05-26 16:46:18 -0400

    Integrate newrelic

--- perma_web/perma/wsgi.py
+++ perma_web/perma/wsgi.py
@@ -9,25 +9,21 @@ this application via the ``WSGI_APPLICATION`` setting.
 """
 import os
 
-import newrelic
-from werkzeug.wsgi import DispatcherMiddleware
+# Newrelic setup
+use_newrelic = os.environ.get("USE_NEW_RELIC", False)
+if use_newrelic:
+    import newrelic.agent
+    newrelic.agent.initialize(os.path.join(os.path.dirname(__file__), '../../services/newrelic/newrelic.ini'))
 
-newrelic.agent.initialize('/srv/www/perma/services/newrelic/newrelic.ini')
-
-# We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
-# if running multiple sites in the same mod_wsgi process. To fix this, use
-# mod_wsgi daemon mode with each site in its own daemon process, or use
-# os.environ["DJANGO_SETTINGS_MODULE"] = "perma.settings"
+# env setup
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "perma.settings")
 os.environ.setdefault("CELERY_LOADER", "django")
 
-# This application object is used by any WSGI server configured to use this
-# file. This includes Django's development server, if the WSGI_APPLICATION
-# setting points here.
+# these imports may depend on env setup and/or newrelic setup that came earlier
+from werkzeug.wsgi import DispatcherMiddleware
 from django.core.wsgi import get_wsgi_application
 from warc_server.app import application as warc_application
 from whitenoise.django import DjangoWhiteNoise
-
 
 # subclass WhiteNoise to add missing mime types
 class PermaWhiteNoise(DjangoWhiteNoise):
@@ -35,10 +31,14 @@ class PermaWhiteNoise(DjangoWhiteNoise):
         self.EXTRA_MIMETYPES += (('image/svg+xml', '.svg'),)
         super(PermaWhiteNoise, self).__init__(*args, **kwargs)
 
-
+# set up application
 application = DispatcherMiddleware(
     PermaWhiteNoise(get_wsgi_application()),  # Django app wrapped with whitenoise to serve static assets
     {
         '/warc': warc_application,  # pywb for record playback
     }
 )
+
+# add newrelic app wrapper
+if use_newrelic:
+    application = newrelic.agent.WSGIApplicationWrapper(application)
