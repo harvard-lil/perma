f2e721e88ef2c643b6761e2d852ab96b2ca3accd
Matt Phillips <mphillips@law.harvard.edu>
2016-05-27 12:26:36 -0400

    Added logic and fields to cache our link counts. Fixes #1402

---
+++ perma_web/perma/migrations/0007_auto_20160527_1625.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('perma', '0006_add_internetarchive_status'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='historicallinkuser',
+            name='link_count',
+            field=models.IntegerField(default=0),
+        ),
+        migrations.AddField(
+            model_name='historicalorganization',
+            name='link_count',
+            field=models.IntegerField(default=0),
+        ),
+        migrations.AddField(
+            model_name='historicalregistrar',
+            name='link_count',
+            field=models.IntegerField(default=0),
+        ),
+        migrations.AddField(
+            model_name='linkuser',
+            name='link_count',
+            field=models.IntegerField(default=0),
+        ),
+        migrations.AddField(
+            model_name='organization',
+            name='link_count',
+            field=models.IntegerField(default=0),
+        ),
+        migrations.AddField(
+            model_name='registrar',
+            name='link_count',
+            field=models.IntegerField(default=0),
+        ),
+        migrations.AlterField(
+            model_name='cdxline',
+            name='link_id',
+            field=models.CharField(max_length=2100, null=True, db_index=True),
+        ),
+    ]
--- perma_web/perma/signals.py
+++ perma_web/perma/signals.py
@@ -1,6 +1,8 @@
 from django.conf import settings
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 
-from .models import LinkUser
+from .models import LinkUser, Link
 
 
 ### read only mode ###
@@ -26,3 +28,44 @@ def read_only_mode(sender, instance, **kwargs):
 
 if settings.READ_ONLY_MODE:
     pre_save.connect(read_only_mode)
+
+@receiver(pre_save, sender=Link)
+def update_link_count(sender, instance, **kwargs):
+    try:
+    # get an existing link
+        loaded_link = sender.objects.get(pk=instance.pk)
+
+        def decrement_link_count(loaded_link):
+            # knock our link counts down by one if we're > 0
+            if loaded_link.created_by.link_count > 0:
+                loaded_link.created_by.link_count -= 1
+                loaded_link.created_by.save()
+
+            if loaded_link.organization.link_count > 0:
+                loaded_link.organization.link_count -= 1
+                loaded_link.organization.save()
+
+            if loaded_link.organization.registrar.link_count > 0:
+                loaded_link.organization.registrar.link_count -= 1
+                loaded_link.organization.registrar.save()
+
+        # subtract from org and registrar if org has changed
+        if loaded_link.organization and loaded_link.organization != instance.organization:
+            decrement_link_count(loaded_link)
+
+        # if the link was deleted
+        if instance.user_deleted and loaded_link.user_deleted != instance.user_deleted:
+            decrement_link_count(loaded_link)    
+
+    except sender.DoesNotExist:
+        # new link. let's add it to the user's sum
+        instance.created_by.link_count += 1
+        instance.created_by.save()
+
+    finally:
+        # if org changed or we have a new link with an associated org, increment
+        if instance.organization and loaded_link.organization != instance.organization:
+            instance.organization.link_count += 1
+            instance.organization.save()
+            instance.organization.registrar.link_count += 1
+            instance.organization.registrar.save()
