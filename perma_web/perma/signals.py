e5ec3a43a07646af125321a7371a7bc23d28ec61
Matt Phillips <mphillips@law.harvard.edu>
2016-05-30 15:47:08 -0400

    A little refinement of logic in link count logic

--- perma_web/perma/signals.py
+++ perma_web/perma/signals.py
@@ -36,25 +36,27 @@ def update_link_count(sender, instance, **kwargs):
         loaded_link = sender.objects.get(pk=instance.pk)
 
         def decrement_link_count(loaded_link):
-            # knock our link counts down by one if we're > 0
-            if loaded_link.created_by.link_count > 0:
-                loaded_link.created_by.link_count -= 1
-                loaded_link.created_by.save()
-
-            if loaded_link.organization and loaded_link.organization.link_count > 0:
-                loaded_link.organization.link_count -= 1
-                loaded_link.organization.save()
+            # minus one from user's organization and related registar
+            if loaded_link.organization:
+                if  loaded_link.organization.link_count > 0:
+                    loaded_link.organization.link_count -= 1
+                    loaded_link.organization.save()
 
                 if loaded_link.organization.registrar.link_count > 0:
                     loaded_link.organization.registrar.link_count -= 1
                     loaded_link.organization.registrar.save()
 
         # subtract from org and registrar if org has changed
-        if loaded_link.organization and loaded_link.organization != instance.organization:
+        if loaded_link.organization != instance.organization:
             decrement_link_count(loaded_link)
 
         # if the link was deleted
         if instance.user_deleted and loaded_link.user_deleted != instance.user_deleted:
+            # minus one from user's link count
+            if loaded_link.created_by.link_count > 0:
+                loaded_link.created_by.link_count -= 1
+                loaded_link.created_by.save()
+
             decrement_link_count(loaded_link)    
 
         # if org changed or we have a new link with an associated org, increment
