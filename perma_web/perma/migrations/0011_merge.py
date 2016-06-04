c9709f9d47a7d72bc4e0af9cb3e35a7cf48f07c5
Jack Cushman <jcushman@gmail.com>
2016-06-04 10:20:24 -0400

    Merge conflicting migrations

---
+++ perma_web/perma/migrations/0011_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('perma', '0010_fix_weekstats'),
+        ('perma', '0010_rename_member_to_user'),
+    ]
+
+    operations = [
+    ]
