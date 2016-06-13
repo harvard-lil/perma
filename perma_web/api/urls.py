dfb83d6fbefc98cfb4d0fa3bd8f03836e4bb71e9
Jack Cushman <jcushman@gmail.com>
2016-06-13 11:47:42 -0400

    Remove unused imports.

--- perma_web/api/urls.py
+++ perma_web/api/urls.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.conf.urls import patterns, url, include
+from django.conf.urls import url, include
 from django.http import HttpResponse
 from django.views.generic.base import RedirectView
 
--- perma_web/perma/urls.py
+++ perma_web/perma/urls.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 from django.views.generic import RedirectView
