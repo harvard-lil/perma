25ab7075a3db636dceb66d2dc3d52ec8d53fc7b6
Anastasia <anastasia.aizman@gmail.com>
2016-07-19 12:13:21 -0400

    fix - setting timegate line

--- perma_web/warc_server/pywb_config.py
+++ perma_web/warc_server/pywb_config.py
@@ -269,7 +269,7 @@ class PermaMementoTimemapView(MementoTimemapView):
     def render_response(self, wbrequest, cdx_lines, **kwargs):
         memento_lines = make_timemap(wbrequest, cdx_lines)
 
-        # new_memento_lines = self.fix_timegate_line(memento_lines)
+        new_memento_lines = self.fix_timegate_line(memento_lines)
 
         response = WbResponse.text_stream(memento_lines, content_type=LINK_FORMAT, )
         response.status_headers.headers.append(('Cache-Control',
