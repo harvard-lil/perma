0433f77f6232465992f103f7748ef58f06747879
Anastasia <anastasia.aizman@gmail.com>
2016-08-22 12:22:48 -0400

    add - slightly more robust airbrake error testing

--- perma_web/spec/javascripts/error-handler.spec.js
+++ perma_web/spec/javascripts/error-handler.spec.js
@@ -2,6 +2,14 @@ describe("Test error-handler.js", function() {
   beforeEach(function() {
     airbrake = undefined;
     jasmine.Ajax.install();
+    jasmine.Ajax.stubRequest('/manage/errors/resolve').andReturn({
+      responseText: 'Issue has been resolved'
+    });
+
+    jasmine.Ajax.stubRequest('/errors/new?').andReturn({
+      responseText: 'Error has been created'
+    });
+
   });
 
   afterEach(function() {
@@ -13,4 +21,23 @@ describe("Test error-handler.js", function() {
     ErrorHandler.init();
     expect(airbrake).toBeDefined();
   });
+
+  it("pings erorr url on notify", function(){
+    ErrorHandler.init();
+    var newErrorObject = airbrake.notify("Horrible Mistake", {});
+    var request = jasmine.Ajax.requests.mostRecent();
+    expect(request.method).toEqual('POST');
+    expect(request.params).toBeDefined();
+  });
+
+  it("pings resolve url on error resolve request", function(){
+    ErrorHandler.init();
+    var newErrorObject = airbrake.notify("Horrible Mistake", {});
+    ErrorHandler.resolve(1);
+    var request = jasmine.Ajax.requests.mostRecent();
+    expect(request.method).toEqual('POST');
+    expect(request.params).toEqual('error_id=1');
+    expect(request.responseText).toEqual('Issue has been resolved');
+  });
+
 });
