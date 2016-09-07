2959ea877cd350bb64f59d075d04993a2e7441cd
Jack Cushman <jcushman@gmail.com>
2016-09-07 14:55:56 -0400

    Fix tests.

--- perma_web/perma/tests/test_permissions.py
+++ perma_web/perma/tests/test_permissions.py
@@ -121,7 +121,7 @@ class PermissionsTestCase(PermaTestCase):
                 for user in view.get('disallowed', all_users - view['allowed']):
                     self.log_in_user(user)
                     resp = self.client.get(url)
-                    self.assertRedirects(resp, settings.LOGIN_URL+"?next="+url,
+                    self.assertRedirects(resp, settings.LOGIN_URL+"?next="+url, target_status_code=302,
                                          msg_prefix="Error while confirming that %s can't view %s: " % (user, view_name))
 
         # make sure that all ^manage/ views were tested
