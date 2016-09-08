ac65a520e0a0f9c511ff19fe3196066a79abf778
Rebecca Cremona <rcremona@law.harvard.edu>
2016-09-08 16:55:28 -0400

    NEW: First draft Firefox extension - bookmarklet clone + branding

---
+++ extras/perma-firefox/bg.js
@@ -0,0 +1,7 @@
+chrome.browserAction.onClicked.addListener(function(tab){
+    chrome.tabs.create({
+        "url": chrome.extension.getURL('https://perma.cc/service/bookmarklet-create/?v=1&url=' + encodeURIComponent(tab.url))
+    });
+});
+
+
