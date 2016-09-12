4f6a8267b39837fc5eae5983fea02e6f866644ea
Jack Cushman <jcushman@gmail.com>
2016-09-12 15:54:50 -0400

    Django 1.10 support

--- perma_web/perma/tests/test_admin.py
+++ perma_web/perma/tests/test_admin.py
@@ -1,10 +1,12 @@
 # Tests for the django admin:
 
-import django_admin_smoke_tests.tests
+from django_admin_smoke_tests.tests import AdminSiteSmokeTestMixin
 from perma.models import LinkUserManager, LinkUser
 from .utils import PermaTestCase
 
-class AdminTestCase(django_admin_smoke_tests.tests.AdminSiteSmokeTest, PermaTestCase):
+class AdminTestCase(AdminSiteSmokeTestMixin, PermaTestCase):
+    fixtures = PermaTestCase.fixtures
+
     def setUp(self):
         # monkey-patch create_superuser, which is relied on by the django_admin_smoke_tests package
         LinkUserManager.create_superuser = lambda *args, **kwargs: LinkUser.objects.get(pk=1)
