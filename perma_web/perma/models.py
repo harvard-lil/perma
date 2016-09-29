dea0ce24e326707d0527166abb025c784091dcbf
Rebecca Cremona <rcremona@law.harvard.edu>
2016-09-29 15:26:37 -0400

    FIX: Registrar tags should not be mandatory

--- perma_web/perma/models.py
+++ perma_web/perma/models.py
@@ -100,7 +100,7 @@ class Registrar(models.Model):
     objects = RegistrarQuerySet.as_manager()
     tracker = FieldTracker()
     history = HistoricalRecords()
-    tags = TaggableManager()
+    tags = TaggableManager(blank=True)
 
     class Meta:
         ordering = ['name']
@@ -207,7 +207,7 @@ class LinkUser(AbstractBaseUser):
         db_index=True,
         error_messages={'unique': u"A user with that email address already exists.",}
     )
-    
+
     registrar = models.ForeignKey(Registrar, blank=True, null=True, related_name='users', help_text="If set, this user is a registrar user. This should not be set if org is set!")
     pending_registrar = models.ForeignKey(Registrar, blank=True, null=True, related_name='pending_users')
     organizations = models.ManyToManyField(Organization, blank=True, related_name='users', help_text="If set, this user is an org user. This should not be set if registrar is set!")
