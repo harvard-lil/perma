9ff6a664b3ee153a017d20177c5500ab0b31cd40
Rebecca Cremona <rcremona@law.harvard.edu>
2016-10-05 16:29:05 -0400

    NEW: Show MAX_ARCHIVE_FILE_SIZE on upload form

--- perma_web/perma/views/link_management.py
+++ perma_web/perma/views/link_management.py
@@ -2,6 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.http import Http404
 from django.shortcuts import get_object_or_404, render
+from django.conf import settings
 
 from ..models import Link, Folder, Organization
 
@@ -50,11 +51,14 @@ def create_link(request):
     else:
         suppress_reminder = request.COOKIES.get('suppress_reminder')
 
+    max_size = settings.MAX_ARCHIVE_FILE_SIZE / 1024 / 1024
+
     return render(request, 'user_management/create-link.html', {
         'this_page': 'create_link',
         'links_remaining': links_remaining,
         'folder': folder,
-        'suppress_reminder': suppress_reminder
+        'suppress_reminder': suppress_reminder,
+        'max_size': max_size
     })
 
 
