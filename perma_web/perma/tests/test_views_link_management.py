f46d08784df4e894567fa59a7032de360ccf0a35
Rebecca Cremona <rcremona@law.harvard.edu>
2016-10-20 09:57:19 -0400

    TEST: browser tool reminders, server side

--- perma_web/perma/tests/test_views_link_management.py
+++ perma_web/perma/tests/test_views_link_management.py
@@ -28,3 +28,20 @@ class LinkManagementViewsTestCase(PermaTestCase):
         messages = list(response.context['messages'])
         self.assertEqual(len(messages), 0)
 
+    def test_reminder(self):
+        response = self.get('create_link',
+                             user = 'test_user@example.com').content
+        self.assertIn("browser-tools-message", response)
+
+    def test_no_reminder_when_refered_from_bookmarklet(self):
+        response = self.get('create_link',
+                             user = 'test_user@example.com',
+                             request_kwargs={'data':{'url':'some-url-here'}}).content
+        self.assertNotIn("browser-tools-message", response)
+
+    def test_no_reminder_when_suppression_cookie_present(self):
+        self.client.cookies.load({'suppress_reminder': 'true'})
+        response = self.get('create_link',
+                             user = 'test_user@example.com')
+        self.assertNotIn("browser-tools-message", response)
+
