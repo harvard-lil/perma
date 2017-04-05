29d8972c586f6fe09a9290edacabbabad5856c96
Jack Cushman <jcushman@law.harvard.edu>
2017-04-05 10:03:04 -0400

    Explicitly check for allowed_update_fields

--- perma_web/api2/serializers.py
+++ perma_web/api2/serializers.py
@@ -13,7 +13,11 @@ class BaseSerializer(serializers.ModelSerializer):
     """ Base serializer from which all of our serializers inherit. """
 
     def update(self, instance, validated_data):
-        """ When updating, requiring that our serializers provide a whitelist of fields that can be updated. """
+        """ 
+            When updating, requiring that our serializers provide a whitelist of fields that can be updated.
+            This is a safety check that avoids implementation errors where users can update fields that should only be set on create.
+        """
+        assert hasattr(self.Meta, 'allowed_update_fields'), "Serializers that are used for update must set Meta.allowed_update_fields"
         if set(validated_data.keys()) - set(self.Meta.allowed_update_fields):
             raise serializers.ValidationError('Only updates on these fields are allowed: %s' % ', '.join(self.Meta.allowed_update_fields))
         return super(BaseSerializer, self).update(instance, validated_data)
