6c6167f35b0a6ed89ed71b4305e5323596c4c7e5
anastasia <anastasia.aizman@gmail.com>
2017-04-17 13:59:13 -0400

    update - writing deleted / inserted / compared html files into static route, serving

---
+++ perma_web/diff/utils.py
@@ -0,0 +1,12 @@
+import re
+from django.conf import settings
+
+protocol = "https://" if settings.SECURE_SSL_REDIRECT else "http://"
+
+def rewrite_html(html_page, guid):
+    tmp_html = re.sub("/{0}".format(guid), "/warc/{0}".format(guid), html_page)
+    return re.sub("http://localhost/warc", "{0}{1}{2}".format(protocol, settings.WARC_HOST, settings.WARC_ROUTE), tmp_html)
+
+def write_to_static(new_string, filename):
+    with open(settings.STATIC_ROOT+filename, 'w+') as f:
+        f.write(new_string)
--- perma_web/diff/views.py
+++ perma_web/diff/views.py
@@ -6,12 +6,15 @@ from perma.models import *
 from api.tests.utils import *
 from django.conf import settings
 from warc_diff_tools.warc_diff_tools import expand_warcs, get_visual_diffs
+import utils
 
 @login_required
 def main(request, guid):
     """
     here, we want to grab the guid coming in, and create a new archive of it
-    we then do a diff on that archive using diff.warc_compare_text.*
+    we then do a diff on that archive using diff.warc_compare_text
+    original archive: archive_one
+    new archive: archive_two
     """
     # limitation: can only compare public links for now
     # maybe this is solved by temporarily allowing users to view, until archive gets deleted or transferred over
@@ -20,43 +23,41 @@ def main(request, guid):
     # move warc over the actual user if they want to keep it
     # all warcs should be deleted forever from diff user every 24 hours
 
-    old_archive = Link.objects.get(guid=guid)
-    # api_url = "http://localhost:8000/api/v1/archives/?api_key=%s" % settings.DIFF_API_KEY
-    # response = requests.post(api_url, data={'url': old_archive.submitted_url})
-    # new_guid = response.json().get('guid')
-    # old_archive = Link.objects.get(guid="AA3S-SQ55")
-    new_archive = Link.objects.get(guid="U75F-W8DK")
+    archive_one = Link.objects.get(guid=guid)
+    api_url = "http://localhost:8000/api/v1/archives/?api_key=%s" % settings.DIFF_API_KEY
+    response = requests.post(api_url, data={'url': archive_one.submitted_url})
+    new_guid = response.json().get('guid')
+    # archive_one = Link.objects.get(guid="AA3S-SQ55")
+    archive_two = Link.objects.get(guid=new_guid)
     max_size = settings.MAX_ARCHIVE_FILE_SIZE / 1024 / 1024
     protocol = "https://" if settings.SECURE_SSL_REDIRECT else "http://"
 
-    warc_one = os.path.join(default_storage.base_location, old_archive.warc_storage_file())
-    warc_two = os.path.join(default_storage.base_location, new_archive.warc_storage_file())
+    warc_one = os.path.join(default_storage.base_location, archive_one.warc_storage_file())
+    warc_two = os.path.join(default_storage.base_location, archive_two.warc_storage_file())
 
-    expanded = expand_warcs(warc_one, warc_two, old_archive.submitted_url, new_archive.submitted_url)
+    expanded_one, expanded_two = expand_warcs(warc_one, warc_two, archive_one.submitted_url, archive_two.submitted_url)
 
-    warc_one_index = old_archive.replay_url(old_archive.submitted_url).data
-    warc_two_index = new_archive.replay_url(new_archive.submitted_url).data
+    html_one = archive_one.replay_url(archive_one.submitted_url).data
+    html_two = archive_two.replay_url(archive_two.submitted_url).data
 
-    deleted, inserted, combined = get_visual_diffs(warc_one_index, warc_two_index)
-    #
-    # with open('original_html.html', 'w+') as f:
-    #     f.write(warc_one_index)
-    #
-    # with open('original_html2.html', 'w+') as f:
-    #     f.write(warc_two_index)
+    rewritten_html_one = utils.rewrite_html(html_one, archive_one.guid)
+    rewritten_html_two = utils.rewrite_html(html_two, archive_two.guid)
 
+    deleted, inserted, combined = get_visual_diffs(rewritten_html_one, rewritten_html_two)
+
+    utils.write_to_static(deleted, '_deleted_{0}_{1}.html'.format(archive_one.guid, archive_two.guid))
+    utils.write_to_static(inserted, '_inserted_{0}_{1}.html'.format(archive_one.guid, archive_two.guid))
+    utils.write_to_static(combined, '_combined_{0}_{1}.html'.format(archive_one.guid, archive_two.guid))
 
     context = {
-        'old_archive': old_archive,
-        'can_view': request.user.can_view(old_archive),
-        'can_delete': request.user.can_delete(old_archive),
-        'can_toggle_private': request.user.can_toggle_private(old_archive),
-        'old_archive_capture': old_archive.primary_capture,
+        'archive_one': archive_one,
+        'archive_two': archive_two,
+        'archive_one_capture': archive_one.primary_capture,
         'this_page': 'single_link',
         'max_size': max_size,
-        'link_url': settings.HOST + '/' + old_archive.guid,
+        'link_url': settings.HOST + '/' + archive_one.guid,
         'protocol': protocol,
-        'new_archive_capture': new_archive.primary_capture,
+        'archive_two_capture': archive_two.primary_capture,
     }
 
 
