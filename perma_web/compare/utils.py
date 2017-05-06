d252b2a744810a51f0dec71938010c57b84d2d5a
anastasia <anastasia.aizman@gmail.com>
2017-05-05 21:00:59 -0400

    add - compare dir and file creation / finding utils

--- perma_web/compare/utils.py
+++ perma_web/compare/utils.py
@@ -1,3 +1,4 @@
+import os
 import re
 from django.conf import settings
 
@@ -7,6 +8,21 @@ def rewrite_html(html_page, guid):
     tmp_html = re.sub("/{0}".format(guid), "/warc/{0}".format(guid), html_page)
     return re.sub("http://localhost/warc", "{0}{1}{2}".format(protocol, settings.WARC_HOST, settings.WARC_ROUTE), tmp_html)
 
-def write_to_static(new_string, filename):
-    with open(settings.STATIC_ROOT+filename, 'w+') as f: 
+def write_to_static(new_string, filename,  old_guid, new_guid):
+    dirpath = get_compare_dir_path(old_guid, new_guid)
+    filepath = os.join(dirpath, filename)
+    with open(filepath, 'w+') as f:
         f.write(new_string)
+
+def create_compare_dir(old_guid, new_guid):
+    dirpath = get_compare_dir_path(old_guid, new_guid)
+    if not os.path.exists(dirpath):
+        os.makedirs(dirpath)
+
+def compare_dir_exists(old_guid, new_guid):
+    dirpath = get_compare_dir_path(old_guid, new_guid)
+    return os.path.exists(dirpath)
+
+def get_compare_dir_path(old_guid, new_guid):
+    dirname = "{0}_compare_{1}".format(old_guid, new_guid)
+    return os.path.join(settings.STATIC_COMPARE_ROOT, dirname)
