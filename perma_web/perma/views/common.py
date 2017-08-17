00083c8628599aae75b74999fbf9a5f130bc30d0
anastasia <anastasia.aizman@gmail.com>
2017-08-17 17:12:35 -0400

    rename

--- perma_web/perma/views/common.py
+++ perma_web/perma/views/common.py
@@ -212,7 +212,7 @@ def single_linky(request, guid):
 
     # Special handling for mobile pdf viewing because it can be buggy
     # Redirecting to a download page if on mobile
-    download_pdf_view = redirect_to_download(capture.mime_type(), raw_user_agent)
+    redirect_to_download_view = redirect_to_download(capture.mime_type(), raw_user_agent)
 
     # If this record was just created by the current user, show them a new record message
     new_record = request.user.is_authenticated() and link.created_by_id == request.user.id and not link.user_deleted \
@@ -228,7 +228,7 @@ def single_linky(request, guid):
 
     context = {
         'link': link,
-        'download_pdf_view': download_pdf_view,
+        'redirect_to_download_view': redirect_to_download_view,
         'mime_type': capture.mime_type(),
         'can_view': request.user.can_view(link),
         'can_edit': request.user.can_edit(link),
