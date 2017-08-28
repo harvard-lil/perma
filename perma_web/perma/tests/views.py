0d23e4ce26d5c5ab792984a925e8a434aed1ae9d
Rebecca Cremona <rcremona@law.harvard.edu>
2017-08-28 16:40:02 -0400

    Change functional test for javascript error catching so it works on Chrome.

--- perma_web/perma/tests/views.py
+++ perma_web/perma/tests/views.py
@@ -1,8 +1,12 @@
 # Views that only load when we are running tests:
 
 from django.http import HttpResponse
+from django.shortcuts import render
 from perma.utils import get_client_ip
 
 
 def client_ip(request):
     return HttpResponse(get_client_ip(request))
+
+def js_error(request):
+    return render(request, 'tests/js-error.html')
