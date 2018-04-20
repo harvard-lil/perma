44b8381b752ee9d275cd6b14390febfdc0ab4074
Ben Steinberg <bsteinberg@law.harvard.edu>
2018-04-20 10:04:03 -0400

    Thought I had corrected indentation already

--- perma_web/perma/templatetags/version.py
+++ perma_web/perma/templatetags/version.py
@@ -6,8 +6,8 @@ register = template.Library()
 
 @register.simple_tag
 def perma_version():
-   try:
-       with open(os.path.join(settings.STATIC_ROOT, 'version.txt'), 'r') as f:
-           return f.read().strip()
-   except IOError:
-       return False
+    try:
+        with open(os.path.join(settings.STATIC_ROOT, 'version.txt'), 'r') as f:
+            return f.read().strip()
+    except IOError:
+        return False
