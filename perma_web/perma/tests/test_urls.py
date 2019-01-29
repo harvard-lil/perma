d714314a139551f58f73ebc32ec595c440916ca3
Rebecca Cremona <rcremona@law.harvard.edu>
2019-01-29 15:53:43 -0500

    Exclude archive-error page from test.

--- perma_web/perma/tests/test_urls.py
+++ perma_web/perma/tests/test_urls.py
@@ -10,9 +10,14 @@ class UrlsTestCase(PermaTestCase):
         A really simple test for 500 errors. We test all views that don't
         take parameters (it's not easy to guess what params they want).
         """
+        exclude = {
+            'archive_error': 'because it returns 500 by default'
+        }
 
         for urlpattern in urlpatterns:
-            if '?P<' not in urlpattern.regex.pattern and urlpattern.name:
+            if '?P<' not in urlpattern.regex.pattern \
+                     and urlpattern.name \
+                     and urlpattern.name not in exclude:
                 response = self.client.get(reverse(urlpattern.name))
                 self.assertNotEqual(response.status_code, 500)
 
