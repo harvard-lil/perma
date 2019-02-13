d7bd05b7cebb41845b0ac6c6b296c1b79089e97c
Rebecca Cremona <rcremona@law.harvard.edu>
2019-02-13 12:42:19 -0500

    First try.

--- perma_web/perma/settings/deployments/settings_travis.py
+++ perma_web/perma/settings/deployments/settings_travis.py
@@ -10,3 +10,14 @@ DATABASES['default']['PASSWORD'] = ''
 DATABASES['perma-cdxline']['HOST'] = '127.0.0.1'
 DATABASES['perma-cdxline']['USER'] = 'root'
 DATABASES['perma-cdxline']['PASSWORD'] = ''
+
+REMOTE_SELENIUM = True
+if REMOTE_SELENIUM:
+    HOST = 'perma.test:8000'
+    PLAYBACK_HOST = 'perma-archives.test:8000'
+
+ENABLE_WR_PLAYBACK = False
+if ENABLE_WR_PLAYBACK:
+    assert REMOTE_SELENIUM, "WR Playback must be tested with REMOTE_SELENIUM = True"
+    WR_API = 'http://nginx:80/api/v1'
+    PLAYBACK_HOST = 'perma-archives:81'
