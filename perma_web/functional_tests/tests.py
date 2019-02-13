cf76c88830a7eb35350b74f25afbc352a547530b
Rebecca Cremona <rcremona@law.harvard.edu>
2019-02-13 13:38:33 -0500

    Perma isn't on Docker network.

--- perma_web/functional_tests/tests.py
+++ perma_web/functional_tests/tests.py
@@ -206,8 +206,12 @@ class FunctionalTest(BaseTestCase):
         options = webdriver.ChromeOptions()
         options.add_argument('headless')
         options.add_argument('window-size=1024x800')
+        # cls.driver = webdriver.Remote(
+        #     command_executor='http://selenium:4444/wd/hub',
+        #     desired_capabilities=options.to_capabilities()
+        # )
         cls.driver = webdriver.Remote(
-            command_executor='http://selenium:4444/wd/hub',
+            command_executor='http://localhost:4444/wd/hub',
             desired_capabilities=options.to_capabilities()
         )
 
