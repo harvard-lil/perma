db17ab0af670cc8678b6410f88a8b847a8348348
Rebecca Cremona <rcremona@law.harvard.edu>
2019-02-20 16:45:32 -0500

    Set up directories for the recorder container via entrypoint.

---
+++ services/docker/webrecorder/docker-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# see https://docs.docker.com/engine/reference/builder/#entrypoint
+# and https://success.docker.com/article/use-a-script-to-initialize-stateful-container-data
+set -e
+
+# Make sure the these directories exist and are owned by "archivist".
+# Must be run as root; configure Docker or Docker-Compose accordingly.
+if [ "$1" = 'uwsgi' ]; then
+    mkdir -p "$RECORD_ROOT"
+    mkdir -p "$STORAGE_ROOT"
+    chown -R archivist:archivist "$RECORD_ROOT"
+    chown -R archivist:archivist "$STORAGE_ROOT"
+fi
+
+# Run the original command, as "archivist".
+exec su -m archivist -c "$*"
