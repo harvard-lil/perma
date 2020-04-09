d1b341d5d026d70566e91cbda92febf7765fda85
Rebecca Cremona <rcremona@law.harvard.edu>
2020-04-09 18:03:03 -0400

    Set a cookie to toggle Cloudflare caching.

--- perma_web/perma/middleware.py
+++ perma_web/perma/middleware.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.http import Http404
 from django.utils.deprecation import MiddlewareMixin
 
@@ -10,3 +11,16 @@ class AdminAuthMiddleware(MiddlewareMixin):
         if request.path.startswith('/admin/') and not getattr(request.user, 'is_staff', False):
             raise Http404
 
+
+def bypass_cache_middleware(get_response):
+
+    def middleware(request):
+        response = get_response(request)
+        if request.user.is_authenticated:
+            response.set_cookie(settings.CACHE_BYPASS_COOKIE_NAME, 'True')
+        else:
+            response.delete_cookie(settings.CACHE_BYPASS_COOKIE_NAME)
+
+        return response
+
+    return middleware
