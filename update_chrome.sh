6db89238136dedf3861cd64e1bfd360cdc4e8336
Rebecca Cremona <rcremona@law.harvard.edu>
2021-04-29 13:16:14 -0400

    Use a cache-busting trick to force Chrome update in image.

--- update_chrome.sh
+++ update_chrome.sh
@@ -5,8 +5,9 @@
 # necessary. Rebuilding is somewhat time-consuming, so only run it when you
 # have reason to think there's a new Chrome version.
 
-docker-compose build --no-cache
-docker-compose up -d
+ORIG_CACHE_BUSTER=`grep chrome-layer-cache-buster docker-compose.yml | awk  '{print $2}'`
+perl -pi -e "s/chrome-layer-cache-buster: .*/chrome-layer-cache-buster: $(uuidgen)/" docker-compose.yml
+docker-compose up -d --build
 LATEST=`docker-compose exec -T web pipenv run google-chrome --version | cut -f 3 -d ' '`
 SETTING=`grep CAPTURE_USER_AGENT perma_web/perma/settings/deployments/settings_common.py | sed 's/.*Chrome\/\([^ ]*\)\(.*\)/\1/'`
 if [ "$LATEST" != "$SETTING" ] ; then
@@ -14,5 +15,7 @@ if [ "$LATEST" != "$SETTING" ] ; then
     COUNTER=`grep perma3 docker-compose.yml | cut -f 2 -d '.'`
     let NEWCOUNTER=$COUNTER+1
     perl -pi -e "s/perma3:0.$COUNTER/perma3:0.$NEWCOUNTER/" docker-compose.yml
+else
+    perl -pi -e "s/chrome-layer-cache-buster: .*/chrome-layer-cache-buster: $ORIG_CACHE_BUSTER/" docker-compose.yml
 fi
 docker-compose down
