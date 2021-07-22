499b129fed2acce3c10f79704ac35bdefbfb7c80
Rebecca Cremona <rcremona@law.harvard.edu>
2021-07-22 13:05:05 -0400

    Re-enable support for WR_DEBUG env var.

---
+++ services/docker/webrecorder/recorder_main.py
@@ -0,0 +1,24 @@
+from gevent import monkey; monkey.patch_all()
+
+import os
+
+from webrecorder.utils import load_wr_config, init_logging, spawn_once, get_bool
+from webrecorder.rec.webrecrecorder import WebRecRecorder
+
+
+# =============================================================================
+def init():
+    init_logging(debug=get_bool(os.environ.get('WR_DEBUG', 'True')))
+
+    config = load_wr_config()
+
+    wr = WebRecRecorder(config)
+
+    spawn_once(wr.msg_listen_loop)
+
+    wr.init_app()
+    wr.app.wr = wr
+
+    return wr.app
+
+
