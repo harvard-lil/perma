5926118cd766ae2a09ced09e90bfe16409a47843
Rebecca Cremona <rcremona@law.harvard.edu>
2022-01-07 19:38:14 -0500

    Seems identicall but faster.

--- migrate.sh
+++ migrate.sh
@@ -5,7 +5,13 @@
 # docker-compose exec pgloader pgloader --with "prefetch rows = 1000" mysql://root:password@mysqldb/perma postgresql://postgres:password@db/postgres
 
 # Second try: init db from Django migrations, copy over data
+# docker-compose exec web pipenv run ./manage.py migrate --noinput
+# docker-compose exec db bash -c "PGPASSWORD=password psql -U perma -d perma -c 'ALTER SCHEMA public RENAME TO perma'"
+# docker-compose exec pgloader pgloader --with "prefetch rows = 1000" --with "create no tables" --with "include no drop" --with "truncate" mysql://root:password@mysqldb/perma postgresql://perma:password@db/perma
+# docker-compose exec db bash -c "PGPASSWORD=password psql -U perma -d perma -c 'ALTER SCHEMA perma RENAME TO public'"
+
+# Third try: init db from Django migrations, copy over data with "data only"
 docker-compose exec web pipenv run ./manage.py migrate --noinput
-docker-compose exec db PGPASSWORD=password psql -U perma -d perma -p password -c 'ALTER SCHEMA public RENAME TO perma'
-docker-compose exec pgloader pgloader --with "prefetch rows = 1000" --with "create no tables" --with "include no drop" --with "truncate" mysql://root:password@mysqldb/perma postgresql://perma:password@db/perma
-docker-compose exec db psql -U perma -d perma -p password -c 'ALTER SCHEMA perma RENAME TO public'
+docker-compose exec db bash -c "PGPASSWORD=password psql -U perma -d perma -c 'ALTER SCHEMA public RENAME TO perma'"
+docker-compose exec pgloader pgloader --with "prefetch rows = 1000" --with "data only" --with "truncate" mysql://root:password@mysqldb/perma postgresql://perma:password@db/perma
+docker-compose exec db bash -c "PGPASSWORD=password psql -U perma -d perma -c 'ALTER SCHEMA perma RENAME TO public'"
