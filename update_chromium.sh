da41d5333dd31a8880e3fede874e23eb18711b70
bensteinberg <bsteinberg@law.harvard.edu>
2022-04-13 11:12:10 -0400

    Update Chromium and update mechanism

--- update_chromium.sh
+++ update_chromium.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 
 # This script rebuilds the Docker containers, installing a new version of
-# Chrome if available, and updates our user agent and image version number if
-# necessary. Rebuilding is somewhat time-consuming, so only run it when you
-# have reason to think there's a new Chrome version.
+# Chromium if specified in the Dockerfile, and updates our user agent and
+# image version number if necessary.
 
-ORIG_CACHE_BUSTER=`grep chrome-layer-cache-buster docker-compose.yml | awk  '{print $2}'`
-perl -pi -e "s/chrome-layer-cache-buster: .*/chrome-layer-cache-buster: $(uuidgen)/" docker-compose.yml
 docker-compose up -d --build
 LATEST=`docker-compose exec -T web bash -c "chromium --version" | cut -f 2 -d ' '`
 SETTING=`grep CAPTURE_USER_AGENT perma_web/perma/settings/deployments/settings_common.py | sed 's/.*Chrome\/\([^ ]*\)\(.*\)/\1/'`
@@ -15,7 +12,5 @@ if [ "$LATEST" != "$SETTING" ] ; then
     COUNTER=`grep perma3 docker-compose.yml | cut -f 2 -d '.'`
     let NEWCOUNTER=$COUNTER+1
     perl -pi -e "s/perma3:0.$COUNTER/perma3:0.$NEWCOUNTER/" docker-compose.yml
-else
-    perl -pi -e "s/chrome-layer-cache-buster: .*/chrome-layer-cache-buster: $ORIG_CACHE_BUSTER/" docker-compose.yml
 fi
 docker-compose down
