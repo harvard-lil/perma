d1bc799d2528e878172039eeabc0ead001e06f89
Rebecca Cremona <rcremona@law.harvard.edu>
2022-04-19 12:49:43 -0400

    Smooth out certificate installation.

--- make_cert.sh
+++ make_cert.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-HOSTS=$(docker compose exec -T web bash -c "echo \"from django.conf import settings; print(' '.join(host for host in settings.ALLOWED_HOSTS))\" | python ./manage.py shell")
+HOSTS=$(docker compose run -T web bash -c "echo \"from django.conf import settings; print(' '.join(host for host in settings.ALLOWED_HOSTS))\" | python ./manage.py shell")
 mkcert -key-file perma_web/perma-test.key -cert-file perma_web/perma-test.crt $HOSTS
-mkcert -key-file services/docker/webrecorder/nginx/perma-archives.key -cert-file services/docker/webrecorder/nginx/perma-archives.crt perma-archives.test
+mkcert -key-file services/docker/webrecorder/nginx/ssl/perma-archives.key -cert-file services/docker/webrecorder/nginx/ssl/perma-archives.crt perma-archives.test
+
+# For dev convenience: restart nginx (which will have crashed immediately) now that the required cert and key files are available
+# (will be a no-op if the nginx servce wasn't started in the first place, i.e., if docker compose isn't currently running `up`)
+docker compose restart nginx
