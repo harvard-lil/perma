b40315e6b9f863b018d634870b3f62907ed56144
Ben Steinberg <ben@potswift.org>
2022-04-27 16:50:39 -0400

    Leave settings untouched if there's a problem getting Chromium

--- update_chromium.sh
+++ update_chromium.sh
@@ -6,7 +6,7 @@
 docker-compose up -d --build
 LATEST=`docker-compose exec -T web bash -c "chromium --version" | cut -f 2 -d ' '`
 SETTING=`grep CAPTURE_USER_AGENT perma_web/perma/settings/deployments/settings_common.py | sed 's/.*Chrome\/\([^ ]*\)\(.*\)/\1/'`
-if [ "$LATEST" != "$SETTING" ] ; then
+if [ "$LATEST" != "$SETTING" ] && [ "$LATEST" != "" ]; then
     perl -pi -e "s/$SETTING/$LATEST/" perma_web/perma/settings/deployments/settings_common.py
 fi
 docker-compose down
