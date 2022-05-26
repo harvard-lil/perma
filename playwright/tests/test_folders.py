1cde326e59e7cded8b93d468564f3dd0885288b3
Liza Daly <lizadaly@gmail.com>
2022-05-26 09:12:56 -0400

    Add coverage extension and extra test

--- playwright/tests/test_folders.py
+++ playwright/tests/test_folders.py
@@ -4,4 +4,21 @@ def test_create_folder(logged_in_user, urls):
     logged_in_user.goto(urls.folders)
     folder_count = logged_in_user.locator('.jstree-last').count()
     logged_in_user.locator('.new-folder').click()
-    assert logged_in_user.locator('.jstree-last').count() == folder_count + 1
+    logged_in_user.locator(f":nth-match(.jstree-last, {folder_count + 1})").wait_for()
+
+def test_delete_folder(logged_in_user, urls):
+    """Clicking the folder delete button should delete an existing empty folder"""
+    logged_in_user.goto(urls.folders)
+
+    # Create a new folder and wait for it to exist
+    folder_count = logged_in_user.locator('.jstree-last').count()
+    logged_in_user.locator('.new-folder').click()
+    new_folder = logged_in_user.locator(f":nth-match(.jstree-last, {folder_count + 1})")
+    new_folder.wait_for()
+
+    # Now delete it
+    new_folder.click(button="right")
+    with logged_in_user.expect_navigation():
+        logged_in_user.click("text=Delete")
+
+    new_folder.wait_for(state="hidden")
