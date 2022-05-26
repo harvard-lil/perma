9950250659a4648279e8d7debc372285791c7402
Liza Daly <lizadaly@gmail.com>
2022-05-26 08:21:10 -0400

    Add more test coverage

--- playwright/tests/test_links.py
+++ playwright/tests/test_links.py
@@ -17,7 +17,15 @@ def test_create_link(logged_in_user) -> None:
 
 def test_link_required(logged_in_user) -> None:
     """A friendly message should be displayed if the field is omitted"""
+    logged_in_user.locator('#rawUrl')
+    logged_in_user.locator('#addlink').click()
+    assert "URL cannot be empty" in logged_in_user.locator("p.message-large").text_content()
+
+def test_upload_nonexistent(logged_in_user) -> None:
+    """A modal should be displayed if the user input a domain we can't resolve"""
     url_field = logged_in_user.locator('#rawUrl')
     url_field.focus()
+    url_field.type("https://fakedomain.fakething/")
     logged_in_user.locator('#addlink').click()
-    expect(logged_in_user.locator("p.message-large")).to_have_text("URL cannot be empty.")
+    assert "Couldn't resolve domain." in logged_in_user.locator("p.message-large").text_content()
+
