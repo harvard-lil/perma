ffb0b2bcd3fad6c1e151aee929a5080df3e30988
Rebecca Cremona <rcremona@law.harvard.edu>
2022-06-21 15:39:30 -0400

    Don't use a web worker in Safari < 16 (or similar)

--- perma_web/replay/views.py
+++ perma_web/replay/views.py
@@ -10,6 +10,7 @@ def iframe(request):
     guid = request.GET.get('guid')
     screenshot = request.GET.get('type', '') == 'screenshot'
     replay_only = request.GET.get('embed', '') == 'replayonly' or screenshot
+    web_worker = not request.GET.get('worker', '') == 'false'
     hidden = request.GET.get('hidden', '') == 'true'
     ondemand = request.GET.get('ondemand', '') == 'true'
     if request.GET.get('target', '') == 'blank':
@@ -25,6 +26,7 @@ def iframe(request):
         context['warc_source_allowed_host'] = urllib.parse.urlparse(link.warc_presigned_url()).netloc
         context['target_url'] = link.screenshot_capture.url if screenshot else link.submitted_url
         context['embed_style'] = 'replayonly' if replay_only else 'default'
+        context['web_worker'] = web_worker
         context['sandbox'] = link.primary_capture.use_sandbox()
         context['hidden'] = hidden
         context['ondemand'] = ondemand
