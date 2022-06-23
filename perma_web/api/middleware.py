2a1f6d898161d216ce636f40a7183e0e86061a19
Matteo Cargnelutti <matteo.cargnelutti@gmail.com>
2022-06-23 11:43:08 -0400

    Update middleware.py
    Comments fix

--- perma_web/api/middleware.py
+++ perma_web/api/middleware.py
@@ -8,21 +8,21 @@ class CORSMiddleware(DjangoCommonMiddleware):
         response = super().process_response(request, response)
 
         host = request.get_host()
-        origin = request.headers.get("Origin")
 
-        if not origin:
+        origin = request.headers.get("Origin")
+        if not origin: # Set origin to `*` if none was provided.
             origin = "*"
 
         if host.startswith("api."):
-            # Always return HTTP 200 for preflight requests in that context (?)
-            # Note: This is a workaround to circumvent auth issues during preflight.
-            if request.method == "OPTIONS":
+            # Force HTTP 200 for preflight requests (?).
+            # This is a temporary workaround to account for the fact that the browser will not pass a `Authorization` header during preflight, which will trigger a 401.
+            if request.method == "OPTIONS" and response.status_code == 401:
                 response.status_code = 200
 
             response["Access-Control-Allow-Origin"] = origin
             response["Access-Control-Allow-Headers"] = "Authorization"
             response["Access-Control-Allow-Methods"] = "*"
             #response["Access-Control-Allow-Credentials"] = "true"
-            # ^ Uncomment if we want to allow fetch() / XMLHttpRequest to implicitly pass credentials (i.e: HTTP auth stored in session).  
+            # ^ Uncomment if we want to allow `fetch()` / `XMLHttpRequest`` to implicitly pass credentials (i.e: HTTP auth stored in session).  
 
         return response
