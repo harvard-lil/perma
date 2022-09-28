fffbd1892f8b44635cbc04f158f430bbddec63e8
Rebecca Cremona <rcremona@law.harvard.edu>
2022-09-28 10:42:08 -0400

    Remove references to WR in init scripts.

--- init.sh
+++ init.sh
@@ -5,7 +5,3 @@ docker-compose exec web fab dev.init_db
 
 # Perma SSL cert
 bash make_cert.sh
-
-# Webrecorder database
-docker-compose exec app python -m webrecorder.admin \
-    -c info@perma.cc public Test123Test123 archivist 'Info at Perma.cc'
--- make_cert.sh
+++ make_cert.sh
@@ -8,10 +8,8 @@ cp "$(mkcert -CAROOT)/rootCA.pem" perma_web/rootCA.pem
 # Generate certs
 HOSTS=$(docker compose run -T web bash -c "echo \"from django.conf import settings; print(' '.join(host for host in settings.ALLOWED_HOSTS))\" | python ./manage.py shell")
 mkcert -key-file perma_web/perma-test.key -cert-file perma_web/perma-test.crt $HOSTS
-mkcert -key-file services/docker/webrecorder/nginx/ssl/perma-archives.key -cert-file services/docker/webrecorder/nginx/ssl/perma-archives.crt perma-archives.test
 mkcert -key-file services/docker/minio/ssl/private.key -cert-file services/docker/minio/ssl/public.crt perma.minio.test
 
-# For dev convenience: restart minio and nginx (which will have crashed immediately) now that the required cert and key files are available
+# For dev convenience: restart minio (which will have crashed immediately) now that the required cert and key files are available
 # (will be a no-op if the services weren't started in the first place, i.e., if docker compose isn't currently running `up`)
-docker compose restart nginx
 docker compose restart minio
