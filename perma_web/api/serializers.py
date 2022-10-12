55d580645cd5fa4d33da3f2e89d03093e170ed3d
kilbergr <kilbergr@users.noreply.github.com>
2022-10-12 10:34:24 -0400

    allow user to update screenshot_view

--- perma_web/api/serializers.py
+++ perma_web/api/serializers.py
@@ -191,8 +191,8 @@ class AuthenticatedLinkSerializer(LinkSerializer):
     organization = OrganizationSerializer(read_only=True)
 
     class Meta(LinkSerializer.Meta):
-        fields = LinkSerializer.Meta.fields + ('notes', 'created_by', 'is_private', 'private_reason', 'user_deleted', 'archive_timestamp', 'organization')
-        allowed_update_fields = ['submitted_title', 'submitted_description', 'notes', 'is_private', 'private_reason']
+        fields = LinkSerializer.Meta.fields + ('notes', 'created_by', 'is_private', 'private_reason', 'user_deleted', 'archive_timestamp', 'organization', 'screenshot_view')
+        allowed_update_fields = ['submitted_title', 'submitted_description', 'notes', 'is_private', 'private_reason', 'screenshot_view']
 
     def get_warc_download_url(self, link):
         if link.warc_size:
