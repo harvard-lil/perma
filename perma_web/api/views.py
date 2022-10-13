817d927b968fe7a2ab84edab444d198a7f1dbaac
Sabelo Mhlambi <sabzo@users.noreply.github.com>
2022-10-13 10:01:10 -0500

    Update views.py
    update error message to use the key "detail"

--- perma_web/api/views.py
+++ perma_web/api/views.py
@@ -693,7 +693,7 @@ class LinkBatchesListView(BaseView):
         if not request.user.is_authenticated:
             raise PermissionDenied()
         if request.content_type != 'application/json':
-            content = {'content-type error': 'content-type must be aplication/json'}
+            content = {'detail': 'content-type must be aplication/json'}
             return Response(content, status=status.HTTP_400_BAD_REQUEST)
         request.data['created_by'] = request.user.pk
 
