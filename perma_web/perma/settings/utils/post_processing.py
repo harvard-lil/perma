b58b20a91a0536ddf06482e08a6eca57a1d87c47
Rebecca Cremona <rcremona@law.harvard.edu>
2023-03-22 16:45:45 -0400

    Offset the scheduling of confirmation tasks.

--- perma_web/perma/settings/utils/post_processing.py
+++ perma_web/perma/settings/utils/post_processing.py
@@ -80,11 +80,11 @@ def post_process_settings(settings):
         },
         'confirm_files_uploaded_to_internet_archive': {
             'task': 'perma.celery_tasks.queue_file_uploaded_confirmation_tasks',
-            'schedule': crontab(minute="*/5"),
+            'schedule': crontab(minute="2-59/5"),
         },
         'confirm_files_deleted_from_internet_archive': {
             'task': 'perma.celery_tasks.queue_file_deleted_confirmation_tasks',
-            'schedule': crontab(minute="*/5"),
+            'schedule': crontab(minute="2-59/5"),
         }
     }
     settings['CELERY_BEAT_SCHEDULE'] = dict(((job, celerybeat_job_options[job]) for job in settings.get('CELERY_BEAT_JOB_NAMES', [])),
