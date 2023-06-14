73205d154c620d9df8ff65217847353b2320f387
Rebecca Cremona <rcremona@law.harvard.edu>
2023-06-14 13:55:27 -0400

    Replay with wacz-exhibitor, behind a feature flag.

--- make_cert.sh
+++ make_cert.sh
@@ -9,7 +9,9 @@ cp "$(mkcert -CAROOT)/rootCA.pem" perma_web/rootCA.pem
 HOSTS=$(docker compose run -T web bash -c "echo \"from django.conf import settings; print(' '.join(host for host in settings.ALLOWED_HOSTS))\" | python ./manage.py shell")
 mkcert -key-file perma_web/perma-test.key -cert-file perma_web/perma-test.crt $HOSTS
 mkcert -key-file services/docker/minio/ssl/private.key -cert-file services/docker/minio/ssl/public.crt perma.minio.test
+mkcert -key-file services/docker/wacz-exhibitor/ssl/private.key -cert-file services/docker/wacz-exhibitor/ssl/public.crt rejouer.perma.test
 
-# For dev convenience: restart minio (which will have crashed immediately) now that the required cert and key files are available
+# For dev convenience: restart minio and wacz-exhibitor (which will have crashed immediately) now that the required cert and key files are available
 # (will be a no-op if the services weren't started in the first place, i.e., if docker compose isn't currently running `up`)
 docker compose restart minio
+docker compose restart wacz-exhibitor
