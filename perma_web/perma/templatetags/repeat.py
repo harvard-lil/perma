95c62ddbdcb0eca97b50de75f97a8cd4b15f3746
Clare Stanton <40405874+clare-stanton@users.noreply.github.com>
2023-06-23 15:52:20 -0400

    remove repeat.py
    not necessary without partner list / map

--- perma_web/perma/templatetags/repeat.py
+++ perma_web/perma/templatetags/repeat.py
@@ -1,10 +1,1 @@
-from django import template
 
-register = template.Library()
-
-@register.filter
-def repeat(value, times):
-    """
-        Given {{ "foo"|repeat:3 }}, prints "foofoofoo"
-    """
-    return value * times
