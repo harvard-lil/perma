c0b35d824d490e52e94ae738fb992e4eadf17b69
Rebecca Cremona <rcremona@law.harvard.edu>
2023-06-30 10:58:24 -0400

    Add a breakdown by org.

--- perma_web/reporting/admin.py
+++ perma_web/reporting/admin.py
@@ -1,15 +1,19 @@
 from django.contrib import admin
-from django.db.models import Count
+from django.db.models import Count, Q
 from django.urls import reverse
 from django.utils.html import format_html
 
-from perma.admin import RegistrarAdmin
+from perma.admin import RegistrarAdmin, RegistrarNameFilter, RegistrarIdFilter
 
-from reporting.models import FederalCourt
+from reporting.models import FederalCourt, FederalCourtOrganization
+
+
+RegistrarNameFilter.title = "Court/Registrar Name"
+RegistrarIdFilter.title = "Court/Registrar ID"
 
 
 class FederalCourtAdmin(RegistrarAdmin):
-    list_display = ['name', 'last_active', 'get_registrar_users', 'org_users_total', 'org_users_distinct', 'orgs', 'link_count']
+    list_display = ['name', 'last_active', 'get_registrar_users', 'org_users_total', 'org_users_distinct', 'orgs', 'orgs_total', 'link_count']
     list_editable = []
     list_filter = []
     ordering = ['manual_sort_order']
@@ -22,7 +26,9 @@ class FederalCourtAdmin(RegistrarAdmin):
     def get_queryset(self, request):
         return super().get_queryset(request).annotate(
             org_user_total=Count('organizations__users'),
-            org_user_distinct=Count('organizations__users', distinct=True)
+            org_user_distinct=Count('organizations__users', distinct=True),
+            active_orgs_count=Count('organizations', filter=Q(organizations__user_deleted=False), distinct=True)
+
         )
 
     def get_registrar_users(self, obj):
@@ -41,8 +47,36 @@ class FederalCourtAdmin(RegistrarAdmin):
     org_users_distinct.short_description = 'org users (distinct)'
 
     def orgs(self, obj):
-        base_url = reverse("admin:perma_organization_changelist")
+        base_url = reverse("admin:reporting_federalcourtorganization_changelist")
+        return format_html('<a href="{}?registrar_id={}&user_deleted__exact=0">{}</a>', base_url, obj.id, obj.active_orgs_count)
+
+    def orgs_total(self, obj):
+        base_url = reverse("admin:reporting_federalcourtorganization_changelist")
         return format_html('<a href="{}?registrar_id={}">{}</a>', base_url, obj.id, obj.orgs_count)
+    orgs_total.short_description = 'orgs (with deleted)'
+
+
+class FederalCourtOrganizationAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    list_display = ['id', 'court', 'name', 'org_users', 'link_count']
+    list_filter = [RegistrarNameFilter, RegistrarIdFilter, 'user_deleted']
+    ordering = ['registrar__manual_sort_order', 'name']
+
+    def court(self, obj):
+        url = reverse("admin:reporting_federalcourt_change", args=(obj.registrar_id,))
+        return format_html('<a href="{}">{}</a>', url, obj.registrar.name)
+    court.short_description = 'Court/Registrar'
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related('registrar').annotate(
+            org_users=Count('users'),
+        )
+
+    def org_users(self, obj):
+        base_url = reverse("admin:perma_linkuser_changelist")
+        return format_html('<a href="{}?org_id={}">{}</a>', base_url, obj.id, obj.org_users)
+
 
 
 admin.site.register(FederalCourt, FederalCourtAdmin)
+admin.site.register(FederalCourtOrganization, FederalCourtOrganizationAdmin)
--- perma_web/reporting/models.py
+++ perma_web/reporting/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from perma.models import Registrar
+from perma.models import Registrar, Organization
 
 # Proxy models for generating changelist pages in the reporting admin. While these superficially match
 # the reporting views, they are Django models based on the real models in `perma` and so
@@ -15,6 +15,7 @@ class FederalCourtManager(models.Manager):
             tags__slug__in=["federal"]
         )
 
+
 class FederalCourt(Registrar):
 
     class Meta:
@@ -22,3 +23,19 @@ class FederalCourt(Registrar):
         verbose_name = "Federal Court"
 
     objects = FederalCourtManager()
+
+
+class FederalCourtOrganizationManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(
+            registrar__in=FederalCourt.objects.all()
+        )
+
+
+class FederalCourtOrganization(Organization):
+
+    class Meta:
+        proxy = True
+        verbose_name = "Federal Court Organization"
+
+    objects = FederalCourtOrganizationManager()
