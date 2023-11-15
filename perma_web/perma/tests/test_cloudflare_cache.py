55aef43d29a5e50d8823a70821dfab34144eab8a
Rebecca Cremona <rcremona@law.harvard.edu>
2023-11-15 15:29:12 -0500

    Convert test_cloudflare_cache.py to pytest syntax.

--- perma_web/perma/tests/test_cloudflare_cache.py
+++ perma_web/perma/tests/test_cloudflare_cache.py
@@ -1,34 +1,27 @@
+"""
+    Ensure that our local cache of Cloudflare IP addresses is up to date.
+"""
+
 import requests
 import os
-from unittest import TestCase
 
 from django.conf import settings
 
-
-
-class CloudflareCacheTestCase(TestCase):
-    """
-        Ensure that our local cache of Cloudflare IP addresses is up to date.
-    """
-    message = """
+message = """
 Cloudflare has updated its list of IP addresses since we last grabbed a
 copy. We rely on an up-to-date list of IP addresses for rate limiting,
 LOCKSS security, and other purposes. Please run `invoke dev.update-cloudflare-cache`
 and commit the updated files.
 """
 
-    def test_cloudflare_ip4_cache(self):
-        ip4 = requests.get('https://www.cloudflare.com/ips-v4').text
+def test_cloudflare_ip4_cache():
+    ip4 = requests.get('https://www.cloudflare.com/ips-v4').text
 
-        with open(os.path.join(settings.CLOUDFLARE_DIR, 'ips-v4')) as ip4_cache:
-            self.assertEqual(set(ip4.split()),
-                             set(ip4_cache.read().split()),
-                             msg=self.message)
+    with open(os.path.join(settings.CLOUDFLARE_DIR, 'ips-v4')) as ip4_cache:
+        assert set(ip4.split()) == set(ip4_cache.read().split()), message
 
-    def test_cloudflare_ip6_cache(self):
-        ip6 = requests.get('https://www.cloudflare.com/ips-v6').text
+def test_cloudflare_ip6_cache():
+    ip6 = requests.get('https://www.cloudflare.com/ips-v6').text
 
-        with open(os.path.join(settings.CLOUDFLARE_DIR, 'ips-v6')) as ip6_cache:
-            self.assertEqual(set(ip6.split()),
-                             set(ip6_cache.read().split()),
-                             msg=self.message)
+    with open(os.path.join(settings.CLOUDFLARE_DIR, 'ips-v6')) as ip6_cache:
+        assert set(ip6.split()) ==  set(ip6_cache.read().split()), message
