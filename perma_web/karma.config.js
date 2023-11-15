896bc1c5bf5704d1baddfd37404676eaecf47e23
Rebecca Cremona <rcremona@law.harvard.edu>
2023-11-15 13:46:36 -0500

    Point JS tests at Playwright's Chromium (hardcoded)

--- perma_web/karma.config.js
+++ perma_web/karma.config.js
@@ -1,6 +1,8 @@
 var webpackConfig = require('./webpack.config.js');
 var path = require("path");
 
+process.env.CHROMIUM_BIN = '/root/.cache/ms-playwright/chromium-1012/chrome-linux/chrome'
+
 module.exports = function(config) {
   config.set({
     basePath: __dirname,
