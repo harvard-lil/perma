b4f77dd863ebb123ff96c60639e003f2df7ed13d
Rebecca Cremona <rcremona@law.harvard.edu>
2023-11-15 15:13:52 -0500

    Convert test_urls.py to pytest syntax.

--- perma_web/perma/tests/test_urls.py
+++ perma_web/perma/tests/test_urls.py
@@ -1,25 +1,23 @@
 from django.urls import reverse
 
 from perma.urls import urlpatterns
-from .utils import PermaTestCase
 
-class UrlsTestCase(PermaTestCase):
 
-    def test_url_status_codes(self):
-        """
-        A really simple test for 500 errors. We test all views that don't
-        take parameters (it's not easy to guess what params they want).
-        """
-        exclude = {
-            'archive_error': 'because it returns 500 by default'
-        }
+def test_url_status_codes(client):
+    """
+    A really simple test for 500 errors. We test all views that don't
+    take parameters (it's not easy to guess what params they want).
+    """
+    exclude = {
+        'archive_error': 'because it returns 500 by default'
+    }
 
-        for urlpattern in urlpatterns:
-            if '?P<' not in urlpattern.pattern._regex \
-                     and urlpattern.name \
-                     and urlpattern.name not in exclude:
-                response = self.client.get(reverse(urlpattern.name))
-                self.assertNotEqual(response.status_code, 500)
+    for urlpattern in urlpatterns:
+        if '?P<' not in urlpattern.pattern._regex \
+                 and urlpattern.name \
+                 and urlpattern.name not in exclude:
+            response = client.get(reverse(urlpattern.name))
+            assert response.status_code != 500
 
-                response = self.client.post(reverse(urlpattern.name))
-                self.assertNotEqual(response.status_code, 500)
+            response = client.post(reverse(urlpattern.name))
+            assert response.status_code != 500
