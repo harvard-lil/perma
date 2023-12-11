d2bb081bb82b3d0edc8268ad49bab5ac1adafd53
Rebecca Cremona <rcremona@law.harvard.edu>
2023-12-11 16:15:18 -0500

    Remove celery tasks smoke test.

--- perma_web/perma/tests/test_celery_tasks.py
+++
@@ -1,9 +0,0 @@
-from django.test import override_settings
-from perma.celery_tasks import update_stats
-
-
-@override_settings(CELERY_ALWAYS_EAGER=True)
-def testUpdateStats(db):
-    # this tests only that the task runs,
-    # not anything about the task itself
-    assert update_stats.delay()
