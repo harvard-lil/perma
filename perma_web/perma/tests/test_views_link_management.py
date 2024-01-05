c8e6bb5f3fb1092d7bd9388d7ba7d11f2e1ac8f6
R I B Z <kilbergr@users.noreply.github.com>
2024-01-05 16:26:57 -0500

    first pass at these

--- perma_web/perma/tests/test_views_link_management.py
+++ perma_web/perma/tests/test_views_link_management.py
@@ -1,71 +1,72 @@
 # -*- coding: utf-8 -*-
+from django.urls import reverse
+
 from mock import patch
 
-from .utils import PermaTestCase
+# from .utils import PermaTestCase
 
 from perma.views.link_management import Link
 
 
-class LinkManagementViewsTestCase(PermaTestCase):
+### create_link function ###
 
-    ### create_link function ###
+def test_display_after_delete_real_link(perma_client, link_user):
+    response = perma_client.get(reverse('create_link'),
+                            as_user=link_user,
+                            secure=True,
+                            data={'deleted':'ABCD-0003'})
+    messages = list(response.context['messages'])
+    assert len(messages) == 1
+    assert str(messages[0]) == "Deleted - Wikipedia"
 
-    def test_display_after_delete_real_link(self):
-        response = self.get('create_link',
-                             user= 'test_user@example.com',
-                             request_kwargs={'data':{'deleted':'ABCD-0003'}})
-        messages = list(response.context['messages'])
-        self.assertEqual(len(messages), 1)
-        self.assertEqual(str(messages[0]), "Deleted - Wikipedia")
+def test_display_after_delete_fake_link(perma_client, link_user):
+    response = perma_client.get(reverse('create_link'),
+                                as_user=link_user,
+                                secure=True,
+                                data={'deleted': 'ZZZZ-ZZZZ'})
+    messages = list(response.context['messages'])
+    assert len(messages) == 0
 
-    def test_display_after_delete_fake_link(self):
-        response = self.get('create_link',
-                             user = 'test_user@example.com',
-                             request_kwargs={'data':{'deleted':'ZZZZ-ZZZZ'}})
-        messages = list(response.context['messages'])
-        self.assertEqual(len(messages), 0)
+def test_reminder(perma_client, link_user):
+    response = perma_client.get(reverse('create_link'),
+                                as_user=link_user,
+                                secure=True).content
+    assert b"browser-tools-message" in response
 
-    def test_reminder(self):
-        response = self.get('create_link',
-                             user = 'test_user@example.com').content
-        self.assertIn(b"browser-tools-message", response)
+def test_no_reminder_when_refered_from_bookmarklet(perma_client, link_user):
+    response = perma_client.get(reverse('create_link'),
+                                as_user=link_user,
+                                secure=True,
+                                data = {'url':'some-url-here'}).content
+    assert b"browser-tools-message" not in response
 
-    def test_no_reminder_when_refered_from_bookmarklet(self):
-        response = self.get('create_link',
-                             user = 'test_user@example.com',
-                             request_kwargs={'data':{'url':'some-url-here'}}).content
-        self.assertNotIn(b"browser-tools-message", response)
-
-    def test_no_reminder_when_suppression_cookie_present(self):
-        self.client.cookies.load({'suppress_reminder': 'true'})
-        response = self.get('create_link',
-                             user = 'test_user@example.com')
-        self.assertNotIn(b"browser-tools-message", response)
-
-    ### user_delete_link function ###
-
-    def test_confirm_delete_unpermitted_link(self):
-        self.get('user_delete_link',
-                  reverse_kwargs={'args':['7CF8-SS4G']},
-                  user = 'test_user@example.com',
-                  require_status_code = 404)
-
-    def test_confirm_delete_nonexistent_link(self):
-        self.get('user_delete_link',
-                  reverse_kwargs={'args':['ZZZZ-ZZZZ']},
-                  user = 'test_user@example.com',
-                  require_status_code = 404)
-
-    # only brand new links can be deleted,
-    # so we have to mock Link.is_permanent to always return false
-    @patch.object(Link, 'is_permanent', lambda x: False)
-    def test_confirm_delete_permitted_link(self):
-        self.get('user_delete_link',
-                  reverse_kwargs={'args':['JJ3S-2Q5N']},
-                  user = 'test_user@example.com')
+def test_no_reminder_when_suppression_cookie_present(perma_client, client, link_user):
+    # client.cookies.load({'suppress_reminder': 'true'})
+    response = perma_client.get(reverse('create_link'),
+                                as_user=link_user,
+                                secure=True)
+    assert b"browser-tools-message" not in response
 
 
+### user_delete_link function ###
 
+def test_confirm_delete_unpermitted_link(perma_client, link_user):
+    response = perma_client.get(reverse('user_delete_link', args=['7CF8-SS4G']),
+                                as_user=link_user,
+                                secure=True)
+    assert 404 == response.status_code
 
+def test_confirm_delete_nonexistent_link(perma_client, link_user):
+    response = perma_client.get(reverse('user_delete_link', args=['ZZZZ-ZZZZ']),
+                                as_user=link_user,
+                                secure=True)
+    assert 404 == response.status_code
 
-
+# only brand new links can be deleted,
+# so we have to mock Link.is_permanent to always return false
+@patch.object(Link, 'is_permanent', lambda x: False)
+def test_confirm_delete_permitted_link(perma_client, link_user):
+    perma_client.get(reverse('user_delete_link', args=['JJ3S-2Q5N']),
+                as_user=link_user,
+                secure=True)
+    # Is this supposed to have an assertion here?
