544b3e8508c610d4c1b3db21c3211d645d4754fe
R I B Z <kilbergr@users.noreply.github.com>
2024-01-19 11:40:54 -0800

    responds to PR comments by making links created by link_user

--- perma_web/perma/tests/test_views_link_management.py
+++ perma_web/perma/tests/test_views_link_management.py
@@ -10,22 +10,22 @@ from perma.views.link_management import Link
 
 ### create_link function ###
 
-def test_display_after_delete_real_link(perma_client, link_user):
+def test_display_after_delete_real_link(perma_client, link_factory, link_user):
+    new_link = link_factory(created_by=link_user)
     response = perma_client.get(reverse('create_link'),
                             as_user=link_user,
-                            data={'deleted':'ABCD-0003'})
+                            data={'deleted': new_link.guid})
     messages = list(response.context['messages'])
     assert len(messages) == 1
-    assert str(messages[0]) == "Deleted - Wikipedia"
-    assert 200 == response.status_code
+    assert str(messages[0]) == f"Deleted - {new_link.submitted_title}"
 
 def test_display_after_delete_fake_link(perma_client, link_user):
+    assert not Link.objects.filter(guid='ZZZZ-ZZZZ').exists()
     response = perma_client.get(reverse('create_link'),
                                 as_user=link_user,
                                 data={'deleted': 'ZZZZ-ZZZZ'})
     messages = list(response.context['messages'])
     assert len(messages) == 0
-    assert 200 == response.status_code
 
 def test_reminder(perma_client, link_user):
     response_content = perma_client.get(reverse('create_link'),
@@ -38,22 +38,22 @@ def test_no_reminder_when_refered_from_bookmarklet(perma_client, link_user):
                                 data = {'url':'some-url-here'}).content
     assert b"browser-tools-message" not in response_content
 
-def test_no_reminder_when_suppression_cookie_present(perma_client, client, link_user):
-    # client.cookies.load({'suppress_reminder': 'true'})
+def test_no_reminder_when_suppression_cookie_present(perma_client, link_user):
+    perma_client.cookies.load({'suppress_reminder': 'true'})
     response = perma_client.get(reverse('create_link'),
                                 as_user=link_user)
     assert b"browser-tools-message" not in response
-    assert 200 == response.status_code
 
 
 ### user_delete_link function ###
 
-def test_confirm_delete_unpermitted_link(perma_client, link_user):
-    response = perma_client.get(reverse('user_delete_link', args=['7CF8-SS4G']),
+def test_confirm_delete_unpermitted_link(perma_client, link, link_user):
+    response = perma_client.get(reverse('user_delete_link', args=[link.guid]),
                                 as_user=link_user)
     assert 404 == response.status_code
 
 def test_confirm_delete_nonexistent_link(perma_client, link_user):
+    assert not Link.objects.filter(guid='ZZZZ-ZZZZ').exists()
     response = perma_client.get(reverse('user_delete_link', args=['ZZZZ-ZZZZ']),
                                 as_user=link_user)
     assert 404 == response.status_code
@@ -61,7 +61,7 @@ def test_confirm_delete_nonexistent_link(perma_client, link_user):
 # only brand new links can be deleted,
 # so we have to mock Link.is_permanent to always return false
 @patch.object(Link, 'is_permanent', lambda x: False)
-def test_confirm_delete_permitted_link(perma_client, link_user):
-    response = perma_client.get(reverse('user_delete_link', args=['JJ3S-2Q5N']),
+def test_confirm_delete_permitted_link(perma_client, link_factory, link_user):
+    new_link = link_factory(created_by=link_user)
+    perma_client.get(reverse('user_delete_link', args=[new_link.guid]),
                 as_user=link_user)
-    assert 404 == response.status_code
