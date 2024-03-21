3bc2f3a5c98fe22e5b76f9b41a6802c6ecf00fd0
tinykite <dakota@tinykitelab.com>
2024-03-21 12:43:35 -0500

    Add app and test vue components

---
+++ perma_web/frontend/components/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+import HelloWorld from './HelloWorld.vue'
+</script>
+
+<template>
+    <HelloWorld />
+</template>
---
+++ perma_web/frontend/components/HelloWorld.vue
@@ -0,0 +1,8 @@
+<script setup>
+import { ref } from 'vue'
+const message = ref('Hello World!')
+</script>
+
+<template>
+    <p class="hello">{{ message }}</p>
+</template>
