a9172e667a72911bd4f4e5d85dfad8f4b13b8129
tinykite <dakota@tinykitelab.com>
2024-04-01 16:13:42 -0500

    Add captureErrorMessage to store

--- perma_web/frontend/stores/globalStore.js
+++ perma_web/frontend/stores/globalStore.js
@@ -2,8 +2,14 @@ import { reactive } from 'vue'
 
 export const globalStore = reactive({
   captureStatus: 'ready',
+
   updateCapture(state) {
-    captureStatus = state
+    this.captureStatus = state
+  },
+
+  captureErrorMessage: '',
+  updateCaptureErrorMessage(message) {
+    this.captureErrorMessage = message
   },
 
   count: 0,
