0af298dc306b148e6388fa2b60ca5b731aa68bd1
tinykite <dakota@tinykitelab.com>
2024-04-01 10:58:44 -0500

    Add captureStatus to global store

--- perma_web/frontend/stores/globalStore.js
+++ perma_web/frontend/stores/globalStore.js
@@ -1,6 +1,11 @@
 import { reactive } from 'vue'
 
 export const globalStore = reactive({
+  captureStatus: 'ready',
+  updateCapture(state) {
+    captureStatus = state
+  },
+
   count: 0,
   increment() {
     this.count++
