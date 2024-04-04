84f5e5d868054b857bb6ac5f68549d1565fac148
tinykite <dakota@tinykitelab.com>
2024-04-04 16:08:49 -0500

    Add spinner component

---
+++ perma_web/frontend/components/Spinner.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import * as Spinner from 'spin.js'
+
+let spinnerRef = ref(null)
+let spinner = new Spinner({ lines: 15, length: 2, width: 2, radius: 9, corners: 0, color: '#2D76EE', trail: 50, top: '-20px' })
+
+onMounted(() => {
+    spinner.spin(spinnerRef.value)
+})
+
+</script>
+<template>
+    <div ref="spinnerRef"></div>
+</template>
