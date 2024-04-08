4c514cc7b6a110eee75daf98195c069685a7325d
tinykite <dakota@tinykitelab.com>
2024-04-08 09:46:49 -0500

    Add top positional prop to Spinner component

--- perma_web/frontend/components/Spinner.vue
+++ perma_web/frontend/components/Spinner.vue
@@ -2,13 +2,16 @@
 import { ref, onMounted } from 'vue'
 import * as Spinner from 'spin.js'
 
+defineProps({
+    top: String,
+})
+
 let spinnerRef = ref(null)
-let spinner = new Spinner({ lines: 15, length: 2, width: 2, radius: 9, corners: 0, color: '#2D76EE', trail: 50, top: '-20px' })
+let spinner = new Spinner({ lines: 15, length: 2, width: 2, radius: 9, corners: 0, color: '#2D76EE', trail: 50, top: top })
 
 onMounted(() => {
     spinner.spin(spinnerRef.value)
 })
-
 </script>
 <template>
     <div ref="spinnerRef"></div>
