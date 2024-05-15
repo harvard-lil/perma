c64044e900a27a483bcd92e82205d9f030086c3c
tinykite <dakota@tinykitelab.com>
2024-05-15 15:43:49 -0500

    Add Dialog component

---
+++ perma_web/frontend/components/Dialog.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { defineProps, ref } from 'vue';
+
+const props = defineProps({
+    handleClick: Function,
+    handleClose: Function,
+})
+
+const dialogRef = ref('')
+
+const handleDialogOpen = () => {
+    dialogRef.value.showModal()
+}
+
+const handleDialogClose = () => {
+    dialogRef.value.close()
+}
+
+defineExpose({
+    handleDialogClose,
+    handleDialogOpen
+});
+
+
+</script>
+
+<template>
+    <dialog class="c-dialog" ref="dialogRef" @click="handleClick" @keydown.esc="handleClose" id="batch-modal">
+        <slot></slot>
+    </dialog>
+</template>
