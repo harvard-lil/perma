a4bb40588853063cab1addd74f55373158b96332
tinykite <dakota@tinykitelab.com>
2024-05-15 15:44:00 -0500

    Add LinkBatchDetails component

---
+++ perma_web/frontend/components/LinkBatchDetails.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    handleClose: Function
+})
+</script>
+
+<template>
+    <div id="batch-details-wrapper">
+        <p id="batch-progress-report"></p>
+        <div id="batch-details" aria-describedby="batch-progress-report"></div>
+        <div class="form-buttons">
+            <button class="btn cancel" @click.prevent="handleClose">Exit</button>
+            <a href="#" id="export-csv" class="btn">Export list as
+                CSV</a>
+        </div>
+    </div>
+</template>
