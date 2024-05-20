aa35cb1770bd00bada8c235ec45b3ecd88fe4ef6
tinykite <dakota@tinykitelab.com>
2024-05-20 10:40:05 -0500

    Remove comments

--- perma_web/frontend/components/CreateLinkBatch.vue
+++ perma_web/frontend/components/CreateLinkBatch.vue
@@ -40,20 +40,17 @@ const handleReset = () => {
 
 const handleClose = () => {
     handleReset()
-    // $(window).trigger("BatchLinkModule.refreshLinkList");
     dialogRef.value.handleDialogClose();
 }
 
 const handleClick = (e) => {
     // Close dialog if users click outside of the inner dialog container
-    // For example, a user may click on the dialog's ::backdrop pseudo-element
+    // For example, clicks on the dialog's ::backdrop pseudo-element will trigger this
     if (e.target.classList.contains('c-dialog')) {
         handleClose();
     }
 }
 
-// Originally called start_batch
-// This is triggered on click
 const handleBatchCaptureRequest = async () => {
     if (!isReady) {
         return
@@ -61,11 +58,6 @@ const handleBatchCaptureRequest = async () => {
 
     globalStore.updateBatchCaptureErrorMessage('')
     globalStore.updateBatchCapture('isValidating')
-
-    //     $input.hide(); // Handled by conditional rendering
-    //     spinner.spin($spinner[0]); // Handled by conditional rendering
-    //     $spinner.removeClass("_hide"); // Handled by conditional rendering
-    //     $loading.focus(); // Not implementing this
 
     const formData = {
         urls: userSubmittedLinks.value.split("\n").map(s => { return s.trim() }).filter(Boolean),
@@ -96,34 +88,9 @@ const handleBatchCaptureRequest = async () => {
         batchCaptureId.value = data // Triggers periodic polling
         globalStore.updateBatchCapture('isQueued')
 
-
-
-        // This is what originally happens after a POST if no error
-        // show_batch(data.id); // This function is no longer needed — it simply updates UI
-
-        // These are the UI updates show_batch tackled 
-        // $batch_details_wrapper.removeClass("_hide"); // Handled with conditional rendering
-
         batchDialogTitle.value = "Link Batch Details"
-        // $batch_modal_title.text("Link Batch Details"); // Handled above
-
         batchCSVUrl.value = `/api/v1/archives/batches/${data.id}/export`
-        // $export_csv.attr("href", `/api/v1/archives/batches/${batch_id}/export`); // Handled above
 
-        // Not sure about this? I thought spinner was already disabled/started, above
-        //     if (!$spinner[0].childElementCount) {
-        //       spinner.spin($spinner[0]);
-        //       $spinner.removeClass("_hide");
-        //      }
-
-        //      let first_time = true; // Handled above
-
-        // show_batch() originally included the retrieve_and_render function, which triggers on an interval
-        // It should be triggered for the first time by setting batchCaptureId.value = data, above
-
-        // Then these functions are also called
-        // populate_link_batch_list() // handle below
-        // $(window).trigger("BatchLinkModule.batchCreated", data.links_remaining); // Handled below
         const batchCreated = new CustomEvent("BatchLinkModule.batchCreated");
         window.dispatchEvent(batchCreated);
 
@@ -137,29 +104,9 @@ const handleError = (error) => {
     globalStore.updateBatchCapture('urlError')
     globalStore.updateBatchCaptureErrorMessage(error)
     console.log(error) // This is a placeholder
-    // Fetch errorMessage
-    // export function showError (jqXHR) {
-    //   var message = getErrorMessage(jqXHR);
-    //   Helpers.informUser(message, 'danger');
-    // }
-
-    // Show error message
-    // export function informUser (message, alertClass) {
-    //   /*
-    //       Show user some information at top of screen.
-    //       alertClass options are 'success', 'info', 'warning', 'danger'
-    //    */
-    //   $('.popup-alert button.close').click();
-    //   alertClass = alertClass || "info";
-    //   $('<div class="alert alert-'+alertClass+' alert-dismissible popup-alert" role="alert" style="display: none">' +
-    //       '<button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>'+
-    //       message +
-    //     '</div>').prependTo('body').fadeIn('fast');
-    // }
     handleClose()
 }
 
-// Originally called retrieve_and_render
 const handleBatchDetailsFetch = async () => {
     console.log('batch details fetch initiated')
     const { data, error, errorMessage } = await useFetch(`/api/v1/archives/batches/${batchCaptureId.value.id}`)
@@ -216,7 +163,6 @@ const handleBatchFormatting = ((captureJobs) => {
     return { allJobs, progressSummary }
 })
 
-// Originally handled in show_batch
 watch(batchCaptureId, () => {
     console.log('batch capture id updated')
     handleBatchDetailsFetch()
@@ -243,10 +189,6 @@ defineExpose({
                 </button>
                 <h3 id="batch-modal-title" class="modal-title">{{ batchDialogTitle }}</h3>
             </div>
-            <!-- <div class="spinner _hide">
-                <span class="sr-only" id="loading" tabindex="-1">Loading</span>
-            </div> -->
-            <!-- // Turns into the below -->
             <template v-if="globalStore.batchCaptureStatus === 'isValidating'">
                 <span class="sr-only">Loading</span>
                 <Spinner isDisabled />
