1defb5f29c6af32f92a0a139be672c99ccb622bb
tinykite <dakota@tinykitelab.com>
2024-06-10 10:05:07 -0500

    Add link for users to log in

--- perma_web/frontend/components/Toast.vue
+++ perma_web/frontend/components/Toast.vue
@@ -9,6 +9,9 @@ const { toasts } = useToast();
         <div v-for="toast in toasts" :key="toast.id" :class="`alert alert-${toast.status} alert-dismissible
         popup-alert`" role="alert">
             {{ toast.message }}
+            <span v-if="toast.message.includes('logged out')">
+                Please <a href='/login' class="u-underline">log in</a> to continue.
+            </span>
             <button type="button" class="close"><span aria-hidden="true">&times;</span><span
                     class="sr-only">Close</span></button>
         </div>
