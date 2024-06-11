03785c2922a276a820ae576481f90f23ca14f7c9
tinykite <dakota@tinykitelab.com>
2024-06-11 15:07:09 -0500

    Add TextInput component

---
+++ perma_web/frontend/components/TextInput.vue
@@ -0,0 +1,15 @@
+<script setup>
+const model = defineModel()
+</script>
+
+<template>
+    <div class="form-group" :class="{
+        'has-error': model.error
+    }">
+        <label class=" control-label" :for="model.value">{{ model.name }} <span v-if="model.description"
+                class="label-instruction">{{
+        model.description }}</span></label>
+        <input v-model="model.value" :id="model.name" :name="model.name" type="text" :placeholder="model.placeholder" />
+        <span v-if="model.error" class="help-block js-warning">{{ model.error }}</span>'
+    </div>
+</template>
