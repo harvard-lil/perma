1ee89a5339def10429fc881834e761df2341e754
tinykite <dakota@tinykitelab.com>
2024-06-11 16:41:38 -0500

    Add separate error prop

--- perma_web/frontend/components/TextInput.vue
+++ perma_web/frontend/components/TextInput.vue
@@ -1,15 +1,19 @@
 <script setup>
 const model = defineModel()
+const props = defineProps({
+    error: String
+})
+
 </script>
 
 <template>
     <div class="form-group" :class="{
-        'has-error': model.error
+        'has-error': props.error
     }">
         <label class=" control-label" :for="model.value">{{ model.name }} <span v-if="model.description"
                 class="label-instruction">{{
         model.description }}</span></label>
         <input v-model="model.value" :id="model.name" :name="model.name" type="text" :placeholder="model.placeholder" />
-        <span v-if="model.error" class="help-block js-warning">{{ model.error }}</span>'
+        <span v-if="props.error" class="help-block js-warning">{{ props.error }}</span>
     </div>
 </template>
