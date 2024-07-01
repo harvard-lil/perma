2b20d18cadf18e426d9d2d371ba9a825cefd685b
tinykite <dakota@tinykitelab.com>
2024-07-01 09:49:44 -0500

    Update feature flag

--- perma_web/frontend/lib/consts.js
+++ perma_web/frontend/lib/consts.js
@@ -1,3 +1,3 @@
 export const validStates = ["pending", "in_progress", "completed"]
 export const transitionalStates = ['pending', 'in_progress']
-export const showDevPlayground = waffle.FLAGS["vue-dashboard"]
+export const showDevPlayground = waffle.FLAGS["developer-playground"]
