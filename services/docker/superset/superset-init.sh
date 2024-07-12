76557b414319cc19773a2b9ef4cbe8f17b497ef4
Ebru Yucesar <eyucesar@law.harvard.edu>
2024-07-12 11:52:48 -0400

    auto connect to perma db at service initialization

--- services/docker/superset/superset-init.sh
+++ services/docker/superset/superset-init.sh
@@ -14,5 +14,8 @@ superset db upgrade
 # set up roles and permissions
 superset init
 
+# create perma db connection if it doesn't exist
+superset set-database-uri -d "$DATABASE_NAME" -u "$SQLALCHEMY_URI" -s
+
 # start server
 /bin/sh -c /usr/bin/run-server.sh
---
+++ services/docker/superset/superset_config.py
@@ -0,0 +1,7 @@
+# enables feature to embed python code into sql queries
+FEATURE_FLAGS = {
+    "ENABLE_TEMPLATE_PROCESSING": True,
+}
+
+# compulsory key
+SECRET_KEY = "abcde"
