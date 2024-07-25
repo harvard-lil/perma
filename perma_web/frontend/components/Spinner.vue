beb196030d3926d893992c5454942691e03a95fd
tinykite <dakota@tinykitelab.com>
2024-07-25 09:55:32 -0500

    Replace isDisabled prop with media feature query

--- perma_web/frontend/components/Spinner.vue
+++ perma_web/frontend/components/Spinner.vue
@@ -1,25 +1,28 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 import * as Spinner from 'spin.js'
+import { usePreferredReducedMotion } from '@vueuse/core'
 
 const props = defineProps({
     top: String,
     length: String,
     color: String,
-    isDisabled: Boolean,
     classList: String,
 })
 
 let spinnerRef = ref(null)
 let spinner = new Spinner({ lines: 15, length: props.length ? parseInt(props.length) : 2, width: 2, radius: 9, corners: 0, color: props.color ? props.color : '#2D76EE', trail: 50, top: props.top ? props.top : '-20px' })
 
+const motionPreferences = usePreferredReducedMotion()
+const showLoader = motionPreferences === 'no-preference'
+
 onMounted(() => {
-    if (!props.isDisabled) {
+    if (showLoader) {
         spinner.spin(spinnerRef.value)
     }
 })
 </script>
 <template>
-    <div v-if="!isDisabled" ref="spinnerRef" :class="!!props.classList && props.classList"></div>
-    <div v-if="isDisabled">Loading</div>
+    <div v-if="showLoader" ref="spinnerRef" :class="!!props.classList && props.classList"></div>
+    <div v-else>Loading</div>
 </template>
