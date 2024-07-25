58fcb8fd8f4540165f93007a7afa882f91eefff4
tinykite <dakota@tinykitelab.com>
2024-07-25 11:08:06 -0500

    Update inputs to accommodate error arrays

--- perma_web/frontend/components/FileInput.vue
+++ perma_web/frontend/components/FileInput.vue
@@ -1,7 +1,7 @@
 <script setup>
 const model = defineModel()
 const props = defineProps({
-    error: String,
+    error: Array,
     id: String
 })
 
@@ -23,6 +23,8 @@ const handleFileChange = (event) => {
         model.description }}</span></label>
         <input v-on:change="handleFileChange" :id="props.id" class="file" :name="props.id" type="file"
             accept=".png,.jpg,.jpeg,.gif,.pdf">
-        <span v-if="props.error" class="help-block js-warning">{{ props.error }}</span>
+        <template v-for="error in props.error">
+            <span class="help-block js-warning">{{ error }}</span>
+        </template>
     </div>
 </template>
