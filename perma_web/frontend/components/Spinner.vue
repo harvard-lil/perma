9fcec7ffb3400204c9ec2a5d3563701bfff00259
tinykite <dakota@tinykitelab.com>
2024-07-31 10:18:52 -0500

    Fix reference

--- perma_web/frontend/components/Spinner.vue
+++ perma_web/frontend/components/Spinner.vue
@@ -14,7 +14,7 @@ let spinnerRef = ref(null)
 let spinner = new Spinner({ lines: 15, length: props.length ? parseInt(props.length) : 2, width: 2, radius: 9, corners: 0, color: props.color ? props.color : '#2D76EE', trail: 50, top: props.top ? props.top : '-20px' })
 
 const motionPreferences = usePreferredReducedMotion()
-const showLoader = motionPreferences === 'no-preference'
+const showLoader = motionPreferences.value === 'no-preference'
 
 onMounted(() => {
     if (showLoader) {
