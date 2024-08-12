07fc18a13388dd6b3db3431868c9b60c495768a0
tinykite <dakota@tinykitelab.com>
2024-08-12 09:45:40 -0500

    Reformat file for readability

--- perma_web/frontend/components/ActionButton.vue
+++ perma_web/frontend/components/ActionButton.vue
@@ -1,5 +1,4 @@
 <script setup>
-
 const props = defineProps({
   name: String,
   handleClick: Function,
@@ -13,18 +12,28 @@ const props = defineProps({
   icon: {
     type: String,
     validator(value) {
-      return ["download-alt", "plus", "edit", "trash", "chevron-down", "chevron-up"].includes(value);
+      return [
+        "download-alt",
+        "plus",
+        "edit",
+        "trash",
+        "chevron-down",
+        "chevron-up",
+      ].includes(value);
     },
-  }
+  },
 });
 
-const isChevron = props.icon === 'chevron-down' || props.icon === 'chevron-up'
-
+const isChevron = props.icon === "chevron-down" || props.icon === "chevron-up";
 </script>
 
 <template>
-  <button :onClick="props.handleClick" 
-  :class="`c-button c-button--icon ${props.iconFamily === 'fontAwesome' ? `icon-${props.icon}` : ''}`">    
+  <button
+    :onClick="props.handleClick"
+    :class="`c-button c-button--icon ${
+      props.iconFamily === 'fontAwesome' ? `icon-${props.icon}` : ''
+    }`"
+  >
     <svg v-if="isChevron" aria-hidden="true" width="18" height="9" viewBox="0 0 18 9" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path v-if="props.icon === 'chevron-down'" d="M8.84746 9L18 2.03226L16.1695 0L8.84746 5.48474L1.83051 7.77811e-08L0 2.03226L8.84746 9Z" fill="#2E76EE"/>
       <path v-if="props.icon === 'chevron-up'" d="M8.84746 0L18 6.96774L16.1695 9L8.84746 3.51526L1.83051 9L0 6.96774L8.84746 0Z" fill="#2E76EE"/>
