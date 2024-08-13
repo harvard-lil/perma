3189d5a996201589919394ae6828a5f7d5bad047
tinykite <dakota@tinykitelab.com>
2024-08-13 10:16:24 -0500

    Add CaptureDetails component

---
+++ perma_web/frontend/components/CaptureDetails.vue
@@ -0,0 +1,15 @@
+<script setup>
+</script>
+
+<template>
+  <div class="container cont-fixed manage-links">
+    <div class="row">
+      <div class="col-md-3">
+        <slot name="leftColumn"></slot>
+      </div>
+      <div class="col-md-9">
+        <slot name="rightColumn"></slot>
+      </div>
+    </div>
+  </div>
+</template>
