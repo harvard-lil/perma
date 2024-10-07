98b0338a6670797766c72f74e9f5eff6c0e690f2
Rebecca Cremona <rcremona@law.harvard.edu>
2024-10-07 16:55:40 -0400

    Convert tests for Registrar add/edit/delete views.

--- perma_web/conftest.py
+++ perma_web/conftest.py
@@ -242,6 +242,12 @@ class RegistrarFactory(DjangoModelFactory):
 class PendingRegistrarFactory(RegistrarFactory):
     status = 'pending'
 
+    pending_users = factory.RelatedFactoryList(
+        'conftest.LinkUserFactory',
+        size=1,
+        factory_related_name='pending_registrar'
+    )
+
 
 @register_factory
 class DeniedRegistrarFactory(RegistrarFactory):
@@ -279,7 +285,7 @@ class LinkUserFactory(DjangoModelFactory):
 
     first_name = factory.Faker('first_name')
     last_name = factory.Faker('last_name')
-    email = factory.Sequence(lambda n: 'user%s@example.com' % n)
+    email = factory.LazyAttribute(lambda o: f'{o.first_name}_{o.last_name}@example.com')
 
     # Default to confirmed and active in the fixtures for convenience
     is_active = True
