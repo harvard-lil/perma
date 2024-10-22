6559ce68446e5ee7fe3938329d9cccd433a471a0
Rebecca Cremona <rcremona@law.harvard.edu>
2024-10-22 15:23:49 -0400

    Lint.

--- perma_web/perma/views/link_management.py
+++ perma_web/perma/views/link_management.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.decorators import login_required
-from django.contrib import messages
 from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 from django.conf import settings
