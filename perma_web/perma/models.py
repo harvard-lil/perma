5e7d49371be3a00fa063822431f3ae0bdfd98eef
Jack Cushman <jcushman@law.harvard.edu>
2024-11-01 10:10:33 -0400

    select_for_update in single query

--- perma_web/perma/models.py
+++ perma_web/perma/models.py
@@ -1842,8 +1842,7 @@ class Link(DeletableModel):
             # Don't let anybody move folders around, until this link is
             # safely inside its destination folder, lest denormalized
             # ownership-related fields get out of sync
-            for folder in itertools.chain(self.folders.all(), [folder]):
-                Folder.objects.select_for_update().get(pk=folder.tree_root_id)
+            Folder.objects.filter(pk_in=itertools.chain(self.folders.all(), [folder])).select_for_update().values_list('id')
 
             # remove this link from any folders it's in for this user
             self.folders.remove(*self.folders.accessible_to(user))
