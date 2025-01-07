637f2439a974630ed1c83be07c7f417721037193
Christopher Setzer <37311998+cmsetzer@users.noreply.github.com>
2025-01-06 16:13:19 -0800

    Don't use getURL to create absolute URL

--- extras/perma-firefox/src/bg.js
+++ extras/perma-firefox/src/bg.js
@@ -1,7 +1,5 @@
-chrome.browserAction.onClicked.addListener(function(tab){
-  chrome.tabs.create({
-    "url": chrome.extension.getURL('https://perma.cc/service/bookmarklet-create/?v=1&url=' + encodeURIComponent(tab.url))
+browser.browserAction.onClicked.addListener(function(tab){
+  browser.tabs.create({
+    "url": "https://perma.cc/service/bookmarklet-create/?v=1&url=" + encodeURIComponent(tab.url)
   });
 });
-
-
