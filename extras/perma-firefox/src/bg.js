0cd47510ba05b881086e0bf9b0e3e5f56f2cf8e9
Christopher Setzer <37311998+cmsetzer@users.noreply.github.com>
2025-02-27 14:52:37 -0800

    Remove old browser extensions

--- extras/perma-firefox/src/bg.js
+++
@@ -1,5 +0,0 @@
-browser.browserAction.onClicked.addListener(function(tab){
-  browser.tabs.create({
-    "url": "https://perma.cc/service/bookmarklet-create/?v=1&url=" + encodeURIComponent(tab.url)
-  });
-});
